CiprianRegus/no-OS-examples

commit 85a3142ff981fd35cd1e00679dcf74dc2a06c298

projects/drivers/xilinx_platform: Wrong SlaveSel value in function call
When writing to SPI, some mask is applied to chip_select value. This seems
to be wrong.

Valid values for ARM platform: "slaves can be selected with values 0-2"
Valid values for Microblaze platform: SlaveMask is a 32-bit mask with a 1
in the bit position of the slave being selected

Change CS values accordingly.

chipSelectIndex valid values(1~8), combined with Valid values for
Microblaze platform (a 32-bit mask with a 1 in the bit position of the
slave being selected) results in another change:
spi_ad_desc->chip_select = spiSettings->chipSelectIndex - 1;

For example:
If spiSettings->chipSelectIndex = 1
then spi_ad_desc->chip_select = 0
XSpi_SetSlaveSelect will be called with 0x01 << 0, value corresponding to
SlaveMask parameter.

Signed-off-by: Cristian Pop <cristian.pop@analog.com>

projects/adrv9009/src/devices/adi_hal/parameters.h
@@ -103,8 +103,8 @@
 #define GPIO_OFFSET		54
 #endif
 
-#define CLK_CS			1
-#define ADRV_CS			2
+#define CLK_CS			0
+#define ADRV_CS			1
 
 #define ADRV_RESETB		GPIO_OFFSET + 52
 #define ADRV_SYSREF_REQ	GPIO_OFFSET + 58

projects/ad9361/src/ad9361_parameters.h
@@ -123,7 +123,7 @@
 #endif
 #endif
 
-#define CLK_CS			0x0f
+#define CLK_CS			0x00
 
 #define RX_CORE_BASEADDR	AD9361_RX_0_BASEADDR
 #define TX_CORE_BASEADDR	AD9361_TX_0_BASEADDR

projects/ad9371/src/devices/adi_hal/common.c
@@ -111,7 +111,7 @@ commonErr_t CMB_SPIWriteByte(spiSettings_t *spiSettings, uint16_t addr,
 {
 	uint8_t buf[3];
 
-	spi_ad_desc->chip_select = spiSettings->chipSelectIndex;
+	spi_ad_desc->chip_select = spiSettings->chipSelectIndex - 1;
 
 	buf[0] = (uint8_t) ((addr >> 8) & 0x7f);
 	buf[1] = (uint8_t) (addr & 0xff);

projects/drivers/xilinx_platform/platform_drivers.c
@@ -259,7 +259,7 @@ int32_t spi_write_and_read(struct spi_desc *desc,
 			   XSPIPS_CLK_PHASE_1_OPTION : 0));
 
 	XSpiPs_SetSlaveSelect(&desc->instance,
-			      0xf & ~desc->chip_select);
+			      desc->chip_select);
 	XSpiPs_PolledTransfer(&desc->instance,
 			      data, data, bytes_number);
 #else
@@ -271,7 +271,7 @@ int32_t spi_write_and_read(struct spi_desc *desc,
 			 XSP_CLK_PHASE_1_OPTION : 0));
 
 	XSpi_SetSlaveSelect(&desc->instance,
-			    desc->chip_select);
+			    0x01 << desc->chip_select);
 
 	XSpi_Transfer(&desc->instance,
 		      data, data, bytes_number);

commit f8ed42cc3705883ebf5488498fee44efbdbc295b

projects:altera: fix chip select value in function
Fixes: 85a3142ff981fd35cd1e00679dcf74dc2a06c298

The updates for chip select values supported by Microblaze/ARM
plafrorms affect the behavior of Altera platform based projects.

The spi impementation for altera platforms is updated to handle the
correct chip select values.

This patch fixes the AD9371 project for altera platform.

CS values for ADRV9009 are updated to match the current altera spi
implementation.

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

projects/adrv9009/src/devices/adi_hal/parameters.h
@@ -54,8 +54,6 @@
 /******************************************************************************/
 #ifdef ALTERA_PLATFORM
 #define GPIO_OFFSET		0
-#define CLK_CS			1
-#define ADRV_CS			2
 
 #define ADRV_RESETB		GPIO_OFFSET + 52
 #define ADRV_SYSREF_REQ	GPIO_OFFSET + 58
@@ -103,9 +101,6 @@
 #define GPIO_OFFSET		54
 #endif
 
-#define CLK_CS			0
-#define ADRV_CS			1
-
 #define ADRV_RESETB		GPIO_OFFSET + 52
 #define ADRV_SYSREF_REQ	GPIO_OFFSET + 58
 #define CLK_RESETB		GPIO_OFFSET + 59
@@ -138,4 +133,8 @@
 
 #define DDR_MEM_BASEADDR		XPAR_DDR_MEM_BASEADDR
 #endif
+
+#define CLK_CS			0
+#define ADRV_CS			1
+
 #endif

projects/drivers/altera_platform/spi.c
@@ -108,7 +108,7 @@ int32_t spi_write_and_read(struct spi_desc *desc,
 		      ALTERA_AVALON_SPI_CONTROL_SSO_MSK);
 	IOWR_32DIRECT(SPI_BASEADDR,
 		      (ALTERA_AVALON_SPI_SLAVE_SEL_REG * 4),
-		      desc->chip_select);
+		      (0x1 << (desc->chip_select)));
 	for (i = 0; i < bytes_number; i++) {
 		while ((IORD_32DIRECT(SPI_BASEADDR,
 				      (ALTERA_AVALON_SPI_STATUS_REG * 4)) &