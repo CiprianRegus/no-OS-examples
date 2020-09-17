CiprianRegus/no-OS-examples

commit 634df049421a29f426dec8bfa92f3a2fb55ee416

ad400x: Update makefile according to gpio changes
Add files for gpio generic interface.
Fixes: 115b501

Signed-off-by: Cristian Pop <cristian.pop@analog.com>

projects/ad400x-fmcz/src.mk
@@ -11,12 +11,13 @@
 
 SRCS := $(PROJECT)/src/ad400x_fmcz.c
 SRCS += $(DRIVERS)/spi/spi.c						\
+	$(DRIVERS)/gpio/gpio.c						\
 	$(DRIVERS)/adc/ad400x/ad400x.c					\
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c				\
 	$(DRIVERS)/axi_core/spi_engine/spi_engine.c			\
 	$(NO-OS)/util/util.c
 SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c					\
-	$(PLATFORM_DRIVERS)/gpio.c					\
+	$(PLATFORM_DRIVERS)/xilinx_gpio.c				\
 	$(PLATFORM_DRIVERS)/xilinx_spi.c				\
 	$(PLATFORM_DRIVERS)/delay.c
 INCS += $(DRIVERS)/adc/ad400x/ad400x.h					\

commit 9636f0c9ebf38a5e20b35d4267eb73382aee037a

projects: ad400x: remove gpio from makefile
The gpio driver is not used by the project, nor the driver.

Fixes: 634df04

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

projects/ad400x-fmcz/src.mk
@@ -11,13 +11,11 @@
 
 SRCS := $(PROJECT)/src/ad400x_fmcz.c
 SRCS += $(DRIVERS)/spi/spi.c						\
-	$(DRIVERS)/gpio/gpio.c						\
 	$(DRIVERS)/adc/ad400x/ad400x.c					\
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c				\
 	$(DRIVERS)/axi_core/spi_engine/spi_engine.c			\
 	$(NO-OS)/util/util.c
 SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c					\
-	$(PLATFORM_DRIVERS)/xilinx_gpio.c				\
 	$(PLATFORM_DRIVERS)/xilinx_spi.c				\
 	$(PLATFORM_DRIVERS)/delay.c
 INCS += $(DRIVERS)/adc/ad400x/ad400x.h					\