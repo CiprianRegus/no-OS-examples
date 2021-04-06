pmallick-adi/no-OS

move spi device_id from extra to spi_desc
This is a plafrom agnostic parameter that from the beggining should
have been in spi_desc

Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

projects/adrv9009/src/devices/adi_hal/no_os_hal.c
@@ -87,6 +87,7 @@ adiHalErr_t ADIHAL_openHw(void *devHalInfo, uint32_t halTimeout_ms)
 
 	status = gpio_get(&dev_hal_data->gpio_adrv_resetb, &gpio_adrv_resetb_param);
 
+	spi_param.id = 0;
 	spi_param.max_speed_hz = 25000000;
 	spi_param.mode = SPI_MODE_0;
 	spi_param.chip_select = dev_hal_data->spi_adrv_csn;

CiprianRegus/no-OS-examples

projects/adrv9009/src/devices/adi_hal/no_os_hal.c
@@ -87,6 +87,7 @@ adiHalErr_t ADIHAL_openHw(void *devHalInfo, uint32_t halTimeout_ms)
 
 	status = gpio_get(&dev_hal_data->gpio_adrv_resetb, &gpio_adrv_resetb_param);
 
+	spi_param.device_id = 0;
 	spi_param.max_speed_hz = 25000000;
 	spi_param.mode = SPI_MODE_0;
 	spi_param.chip_select = dev_hal_data->spi_adrv_csn;