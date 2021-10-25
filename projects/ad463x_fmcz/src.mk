analogdevicesinc/no-OS

projects: ad463x_fmc: initial commit
Add ad463x_fmc project and makefiles.

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

projects/ad463x_fmcz/src.mk
@@ -1,0 +1,44 @@
+################################################################################
+#									       #
+#     Shared variables:							       #
+#	- PROJECT							       #
+#	- DRIVERS							       #
+#	- INCLUDE							       #
+#	- PLATFORM_DRIVERS						       #
+#	- NO-OS								       #
+#									       #
+################################################################################
+
+SRCS := $(PROJECT)/src/ad463x_fmc.c
+SRCS += $(DRIVERS)/spi/spi.c						\
+	$(DRIVERS)/gpio/gpio.c						\
+	$(DRIVERS)/adc/ad463x/ad463x.c					\
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c				\
+	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c		\
+	$(DRIVERS)/axi_core/axi_pwmgen/axi_pwm.c			\
+	$(DRIVERS)/axi_core/spi_engine/spi_engine.c			\
+	$(NO-OS)/util/util.c
+SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c					\
+	$(PLATFORM_DRIVERS)/xilinx_gpio.c				\
+	$(PLATFORM_DRIVERS)/xilinx_spi.c				\
+	$(PLATFORM_DRIVERS)/delay.c
+INCS += $(PROJECT)/src/parameters.h
+INCS += $(DRIVERS)/adc/ad463x/ad463x.h					\
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h				\
+	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.h		\
+	$(DRIVERS)/axi_core/axi_pwmgen/axi_pwm_extra.h			\
+	$(DRIVERS)/axi_core/spi_engine/spi_engine.h			\
+	$(DRIVERS)/axi_core/spi_engine/spi_engine_private.h
+INCS +=	$(PLATFORM_DRIVERS)/spi_extra.h					\
+	$(PLATFORM_DRIVERS)/irq_extra.h					\
+	$(PLATFORM_DRIVERS)/uart_extra.h				\
+	$(PLATFORM_DRIVERS)/gpio_extra.h
+INCS +=	$(INCLUDE)/axi_io.h						\
+	$(INCLUDE)/spi.h						\
+	$(INCLUDE)/pwm.h						\
+	$(INCLUDE)/gpio.h						\
+	$(INCLUDE)/error.h						\
+	$(INCLUDE)/delay.h						\
+	$(INCLUDE)/irq.h						\
+	$(INCLUDE)/uart.h						\
+	$(INCLUDE)/util.h

projects/ad463x_fmcz/src/ad463x_fmc.c
@@ -1,0 +1,192 @@
+/***************************************************************************//**
+ *   @file   ad463x_fmc.c
+ *   @brief  Implementation of Main Function.
+ *   @author Antoniu Miclaus (antoniu.miclaus@analog.com)
+ ********************************************************************************
+ * Copyright 2021(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include <stdio.h>
+#include <inttypes.h>
+#include <xil_cache.h>
+#include "spi_engine.h"
+#include "pwm.h"
+#include "axi_pwm_extra.h"
+#include "ad463x.h"
+#include "error.h"
+#include "clk_axi_clkgen.h"
+#include "gpio.h"
+#include "gpio_extra.h"
+#include "parameters.h"
+
+/******************************************************************************/
+/********************** Macros and Constants Definitions **********************/
+/******************************************************************************/
+#define AD463x_EVB_SAMPLE_NO		1000
+
+/* Main function */
+int main()
+{
+	uint32_t buf[AD463x_EVB_SAMPLE_NO] __attribute__ ((aligned));
+	struct ad463x_dev *dev;
+	int32_t ret;
+	uint32_t i;
+
+	struct spi_engine_offload_init_param spi_engine_offload_init_param = {
+		.offload_config = OFFLOAD_RX_EN,
+		.rx_dma_baseaddr = AD463x_DMA_BASEADDR,
+	};
+
+	struct spi_engine_init_param spi_eng_init_param  = {
+		.ref_clk_hz = 160000000,
+		.type = SPI_ENGINE,
+		.spi_engine_baseaddr = AD463x_SPI_ENGINE_BASEADDR,
+		.cs_delay = 0,
+		.data_width = 24,
+	};
+
+	struct axi_clkgen_init clkgen_init = {
+		.name = "rx_clkgen",
+		.base = RX_CLKGEN_BASEADDR,
+		.parent_rate = 100000000,
+	};
+
+	struct axi_pwm_init_param axi_pwm_init = {
+		.base_addr = AXI_PWMGEN_BASEADDR,
+		.ref_clock_Hz = 100000000,
+	};
+
+	struct pwm_init_param trigger_pwm_init = {
+		.period_ns = 500,	/* 2Mhz */
+		.duty_cycle_ns = AD463X_TRIGGER_PULSE_WIDTH_NS,
+		.polarity = PWM_POLARITY_HIGH,
+		.extra = &axi_pwm_init,
+	};
+
+	struct xil_gpio_init_param gpio_extra_param = {
+		.device_id = GPIO_DEVICE_ID,
+		.type = GPIO_PS,
+	};
+
+	struct gpio_init_param ad463x_resetn = {
+		.number = GPIO_RESETN_1,
+		.platform_ops = &xil_gpio_ops,
+		.extra = &gpio_extra_param
+	};
+
+	struct spi_init_param spi_init = {
+		.chip_select = AD463x_SPI_CS,
+		.max_speed_hz = 80000000,
+		.mode = SPI_MODE_0,
+		.platform_ops = &spi_eng_platform_ops,
+		.extra = (void*)&spi_eng_init_param,
+	};
+
+	struct ad463x_init_param ad463x_init_param = {
+		.spi_init = &spi_init,
+		.offload_init_param = &spi_engine_offload_init_param,
+		.trigger_pwm_init = &trigger_pwm_init,
+		.gpio_resetn = &ad463x_resetn,
+		.clkgen_init = &clkgen_init,
+		.axi_clkgen_rate = 160000000,
+		.reg_access_speed = 20000000,
+		.reg_data_width = 8,
+		.output_mode = AD463X_32_PATTERN,
+		.lane_mode = AD463X_TWO_LANES_PER_CH,
+		.clock_mode = AD463X_SPI_COMPATIBLE_MODE,
+		.data_rate = AD463X_SDR_MODE,
+		.device_id = ID_AD4630_24, /* dev_id */
+		.dcache_invalidate_range =
+		(void (*)(uint32_t, uint32_t))Xil_DCacheInvalidateRange,
+	};
+
+	ret = ad463x_init(&dev, &ad463x_init_param);
+	if (ret != SUCCESS) {
+		printf("AD463x Initialization failed!");
+		return ret;
+	}
+
+	printf("AD463x Successfully initialized!");
+
+	/* Exit register configuration mode */
+	ret = ad463x_exit_reg_cfg_mode(dev);
+	if (ret != SUCCESS)
+		return ret;
+
+	/* Test Pattern Mode, 32-bit output data, 2 lanes per channel */
+	ret = ad463x_read_data(dev, buf, AD463x_EVB_SAMPLE_NO);
+	if (ret != SUCCESS)
+		return ret;
+
+	for (i = 0; i < (AD463x_EVB_SAMPLE_NO / 2); i++)
+		if (buf[i] != AD463X_OUT_DATA_PAT) {
+			printf("AD463x Test Pattern Data read failed!");
+			return FAILURE;
+		}
+
+	printf("AD463x Test Pattern Data successfully read!");
+
+	ad463x_remove(dev);
+
+	ad463x_init_param.output_mode = AD463X_24_DIFF;
+
+	ret = ad463x_init(&dev, &ad463x_init_param);
+	if (ret != SUCCESS)
+		return ret;
+
+	/* Exit register configuration mode */
+	ret = ad463x_exit_reg_cfg_mode(dev);
+	if (ret != SUCCESS)
+		return ret;
+
+	/* Read data */
+	while (true) {
+		ret = ad463x_read_data(dev, buf, AD463x_EVB_SAMPLE_NO);
+		if (ret != SUCCESS)
+			return ret;
+		for (i = 0; i < AD463x_EVB_SAMPLE_NO; i+=2)
+			printf("ADC sample ch1: %lu : %lu \n", i, buf[i]);
+		for (i = 1; i < AD463x_EVB_SAMPLE_NO; i+=2)
+			printf("ADC sample ch2: %lu : %lu \n", i-1, buf[i]);
+	}
+
+	ad463x_remove(dev);
+
+	printf("Done.\n");
+
+	return SUCCESS;
+}

CiprianRegus/no-OS-examples

projects/ad463x_fmcz/src.mk
@@ -1,0 +1,45 @@
+################################################################################
+#									       #
+#     Shared variables:							       #
+#	- PROJECT							       #
+#	- DRIVERS							       #
+#	- INCLUDE							       #
+#	- PLATFORM_DRIVERS						       #
+#	- NO-OS								       #
+#									       #
+################################################################################
+
+SRCS := $(PROJECT)/src/ad463x_fmc.c
+SRCS += $(DRIVERS)/spi/spi.c						\
+	$(DRIVERS)/gpio/gpio.c						\
+	$(DRIVERS)/adc/ad463x/ad463x.c					\
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c				\
+	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c		\
+	$(DRIVERS)/axi_core/axi_pwmgen/axi_pwm.c			\
+	$(DRIVERS)/axi_core/spi_engine/spi_engine.c			\
+	$(NO-OS)/util/util.c
+SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c					\
+	$(PLATFORM_DRIVERS)/xilinx_gpio.c				\
+	$(PLATFORM_DRIVERS)/xilinx_spi.c				\
+	$(PLATFORM_DRIVERS)/delay.c
+INCS += $(PROJECT)/src/parameters.h
+INCS += $(DRIVERS)/adc/ad463x/ad463x.h					\
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h				\
+	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.h		\
+	$(DRIVERS)/axi_core/axi_pwmgen/axi_pwm_extra.h			\
+	$(DRIVERS)/axi_core/spi_engine/spi_engine.h			\
+	$(DRIVERS)/axi_core/spi_engine/spi_engine_private.h
+INCS +=	$(PLATFORM_DRIVERS)/spi_extra.h					\
+	$(PLATFORM_DRIVERS)/irq_extra.h					\
+	$(PLATFORM_DRIVERS)/uart_extra.h				\
+	$(PLATFORM_DRIVERS)/gpio_extra.h
+INCS +=	$(INCLUDE)/axi_io.h						\
+	$(INCLUDE)/spi.h						\
+	$(INCLUDE)/pwm.h						\
+	$(INCLUDE)/gpio.h						\
+	$(INCLUDE)/error.h						\
+	$(INCLUDE)/delay.h						\
+	$(INCLUDE)/irq.h						\
+	$(INCLUDE)/uart.h						\
+	$(INCLUDE)/util.h						\
+	$(INCLUDE)/print_log.h

projects/ad463x_fmcz/src/ad463x_fmc.c
@@ -1,0 +1,193 @@
+/***************************************************************************//**
+ *   @file   ad463x_fmc.c
+ *   @brief  Implementation of Main Function.
+ *   @author Antoniu Miclaus (antoniu.miclaus@analog.com)
+ ********************************************************************************
+ * Copyright 2021(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include <stdio.h>
+#include <inttypes.h>
+#include <xil_cache.h>
+#include "spi_engine.h"
+#include "pwm.h"
+#include "axi_pwm_extra.h"
+#include "ad463x.h"
+#include "error.h"
+#include "clk_axi_clkgen.h"
+#include "gpio.h"
+#include "gpio_extra.h"
+#include "parameters.h"
+#include "print_log.h"
+
+/******************************************************************************/
+/********************** Macros and Constants Definitions **********************/
+/******************************************************************************/
+#define AD463x_EVB_SAMPLE_NO		1000
+
+/* Main function */
+int main()
+{
+	uint32_t buf[AD463x_EVB_SAMPLE_NO] __attribute__ ((aligned));
+	struct ad463x_dev *dev;
+	int32_t ret;
+	uint32_t i;
+
+	struct spi_engine_offload_init_param spi_engine_offload_init_param = {
+		.offload_config = OFFLOAD_RX_EN,
+		.rx_dma_baseaddr = AD463x_DMA_BASEADDR,
+	};
+
+	struct spi_engine_init_param spi_eng_init_param  = {
+		.ref_clk_hz = 160000000,
+		.type = SPI_ENGINE,
+		.spi_engine_baseaddr = AD463x_SPI_ENGINE_BASEADDR,
+		.cs_delay = 0,
+		.data_width = 24,
+	};
+
+	struct axi_clkgen_init clkgen_init = {
+		.name = "rx_clkgen",
+		.base = RX_CLKGEN_BASEADDR,
+		.parent_rate = 100000000,
+	};
+
+	struct axi_pwm_init_param axi_pwm_init = {
+		.base_addr = AXI_PWMGEN_BASEADDR,
+		.ref_clock_Hz = 100000000,
+	};
+
+	struct pwm_init_param trigger_pwm_init = {
+		.period_ns = 500,	/* 2Mhz */
+		.duty_cycle_ns = AD463X_TRIGGER_PULSE_WIDTH_NS,
+		.polarity = PWM_POLARITY_HIGH,
+		.extra = &axi_pwm_init,
+	};
+
+	struct xil_gpio_init_param gpio_extra_param = {
+		.device_id = GPIO_DEVICE_ID,
+		.type = GPIO_PS,
+	};
+
+	struct gpio_init_param ad463x_resetn = {
+		.number = GPIO_RESETN_1,
+		.platform_ops = &xil_gpio_ops,
+		.extra = &gpio_extra_param
+	};
+
+	struct spi_init_param spi_init = {
+		.chip_select = AD463x_SPI_CS,
+		.max_speed_hz = 80000000,
+		.mode = SPI_MODE_0,
+		.platform_ops = &spi_eng_platform_ops,
+		.extra = (void*)&spi_eng_init_param,
+	};
+
+	struct ad463x_init_param ad463x_init_param = {
+		.spi_init = &spi_init,
+		.offload_init_param = &spi_engine_offload_init_param,
+		.trigger_pwm_init = &trigger_pwm_init,
+		.gpio_resetn = &ad463x_resetn,
+		.clkgen_init = &clkgen_init,
+		.axi_clkgen_rate = 160000000,
+		.reg_access_speed = 20000000,
+		.reg_data_width = 8,
+		.output_mode = AD463X_32_PATTERN,
+		.lane_mode = AD463X_TWO_LANES_PER_CH,
+		.clock_mode = AD463X_SPI_COMPATIBLE_MODE,
+		.data_rate = AD463X_SDR_MODE,
+		.device_id = ID_AD4630_24, /* dev_id */
+		.dcache_invalidate_range =
+		(void (*)(uint32_t, uint32_t))Xil_DCacheInvalidateRange,
+	};
+
+	ret = ad463x_init(&dev, &ad463x_init_param);
+	if (ret != SUCCESS) {
+		pr_err("AD463x Initialization failed!");
+		return ret;
+	}
+
+	pr_info("AD463x Successfully initialized!");
+
+	/* Exit register configuration mode */
+	ret = ad463x_exit_reg_cfg_mode(dev);
+	if (ret != SUCCESS)
+		return ret;
+
+	/* Test Pattern Mode, 32-bit output data, 2 lanes per channel */
+	ret = ad463x_read_data(dev, buf, AD463x_EVB_SAMPLE_NO);
+	if (ret != SUCCESS)
+		return ret;
+
+	for (i = 0; i < (AD463x_EVB_SAMPLE_NO / 2); i++)
+		if (buf[i] != AD463X_OUT_DATA_PAT) {
+			pr_err("AD463x Test Pattern Data read failed!");
+			return FAILURE;
+		}
+
+	pr_info("AD463x Test Pattern Data successfully read!");
+
+	ad463x_remove(dev);
+
+	ad463x_init_param.output_mode = AD463X_24_DIFF;
+
+	ret = ad463x_init(&dev, &ad463x_init_param);
+	if (ret != SUCCESS)
+		return ret;
+
+	/* Exit register configuration mode */
+	ret = ad463x_exit_reg_cfg_mode(dev);
+	if (ret != SUCCESS)
+		return ret;
+
+	/* Read data */
+	while (true) {
+		ret = ad463x_read_data(dev, buf, AD463x_EVB_SAMPLE_NO);
+		if (ret != SUCCESS)
+			return ret;
+		for (i = 0; i < AD463x_EVB_SAMPLE_NO; i+=2)
+			pr_info("ADC sample ch1: %lu : %lu \n", i, buf[i]);
+		for (i = 1; i < AD463x_EVB_SAMPLE_NO; i+=2)
+			pr_info("ADC sample ch2: %lu : %lu \n", i-1, buf[i]);
+	}
+
+	ad463x_remove(dev);
+
+	pr_info("Done.\n");
+
+	return SUCCESS;
+}