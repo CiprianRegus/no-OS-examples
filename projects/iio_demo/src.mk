CiprianRegus/no-OS-examples

commit 98f4e11aed6537fec7e9c628314697e34bd25916

iio_demo build fix using new irq_platform_ops

projects/iio_demo/src.mk
@@ -10,7 +10,8 @@ SRCS +=	$(NO-OS)/util/list.c					\
 SRCS += $(DRIVERS)/adc/adc_demo/adc_demo.c				\
 	$(DRIVERS)/adc/adc_demo/iio_adc_demo.c				\
 	$(DRIVERS)/dac/dac_demo/iio_dac_demo.c				\
-	$(DRIVERS)/dac/dac_demo/dac_demo.c
+	$(DRIVERS)/dac/dac_demo/dac_demo.c                              \
+	$(DRIVERS)/irq/irq.c
 
 INCS += $(INCLUDE)/fifo.h					\
 	$(INCLUDE)/uart.h						\
@@ -48,8 +49,8 @@ INCS	 += $(INCLUDE)/timer.h		\
 		$(PLATFORM_DRIVERS)/rtc_extra.h
 endif
 
-SRCS += $(PLATFORM_DRIVERS)/uart.c					\
-	$(PLATFORM_DRIVERS)/irq.c
+SRCS += $(PLATFORM_DRIVERS)/uart.c \
+		$(PLATFORM_DRIVERS)/$(PLATFORM)_irq.c
 
 INCS += $(INCLUDE)/irq.h						\
 	$(INCLUDE)/rtc.h						\

commit ec8fd22df79df58844b3546027d6011583c683c7

fix iio_demo for stm32
98f4e11ae broke this project by adding irq.c in common area in
src.mk. That file is used only on xilinx and aducm3029 in this
project, and not on stm32.

Signed-off-by: Darius Berghe <darius.berghe@analog.com>

projects/iio_demo/src.mk
@@ -10,8 +10,7 @@ SRCS +=	$(NO-OS)/util/list.c \
 SRCS += $(DRIVERS)/adc/adc_demo/adc_demo.c \
 	$(DRIVERS)/adc/adc_demo/iio_adc_demo.c \
 	$(DRIVERS)/dac/dac_demo/iio_dac_demo.c \
-	$(DRIVERS)/dac/dac_demo/dac_demo.c \
-	$(DRIVERS)/irq/irq.c
+	$(DRIVERS)/dac/dac_demo/dac_demo.c
 
 INCS += $(INCLUDE)/fifo.h \
 	$(INCLUDE)/uart.h \
@@ -25,7 +24,8 @@ INCS += $(DRIVERS)/adc/adc_demo/iio_adc_demo.h \
 		$(DRIVERS)/adc/adc_demo/adc_demo.h \
 
 ifeq ($(PLATFORM),$(filter $(PLATFORM),xilinx aducm3029))
-SRCS += $(PLATFORM_DRIVERS)/delay.c
+SRCS += $(PLATFORM_DRIVERS)/delay.c \
+	$(DRIVERS)/irq/irq.c
 endif
 INCS += $(INCLUDE)/delay.h
 