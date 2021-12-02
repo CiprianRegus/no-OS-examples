CiprianRegus/no-OS-examples

commit 737663eab12783c1702d07a2d90b58ede3680664

drivers/api : Platform ops restructured
-i2c, spi, irq and gpio api drivers moved from drivers/driver_type to
drivers/api
-makefiles updated
-minor astyle fix gpio.c

Signed-off-by: Andrei Porumb <andrei.porumb@analog.com>

projects/ad469x_fmcz/src.mk
@@ -10,8 +10,8 @@
 ################################################################################
 
 SRCS += $(PROJECT)/src/ad469x_fmcz.c
-SRCS += $(DRIVERS)/spi/spi.c \
-	$(DRIVERS)/gpio/gpio.c \
+SRCS += $(DRIVERS)/api/spi.c \
+	$(DRIVERS)/api/gpio.c \
 	$(DRIVERS)/adc/ad469x/ad469x.c \
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c \
 	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c \
@@ -26,7 +26,7 @@ ifeq (y,$(strip $(TINYIIOD)))
 LIBRARIES += iio
 SRCS += $(PLATFORM_DRIVERS)/uart.c \
 	$(PLATFORM_DRIVERS)/$(PLATFORM)_irq.c \
-	$(DRIVERS)/irq/irq.c \
+	$(DRIVERS)/api/irq.c \
 	$(NO-OS)/util/fifo.c \
 	$(NO-OS)/util/list.c						
 endif

projects/ad400x-fmcz/src.mk
@@ -10,7 +10,7 @@
 ################################################################################
 
 SRCS += $(PROJECT)/src/ad400x_fmcz.c
-SRCS += $(DRIVERS)/spi/spi.c \
+SRCS += $(DRIVERS)/api/spi.c \
 	$(DRIVERS)/adc/ad400x/ad400x.c \
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c \
 	$(DRIVERS)/axi_core/spi_engine/spi_engine.c \

projects/ad4110/src.mk
@@ -10,9 +10,9 @@
 ################################################################################
 
 SRC_DIRS += $(PROJECT)/src
-SRCS += $(DRIVERS)/spi/spi.c \
-	$(DRIVERS)/irq/irq.c \
-	$(DRIVERS)/gpio/gpio.c \
+SRCS += $(DRIVERS)/api/spi.c \
+	$(DRIVERS)/api/irq.c \
+	$(DRIVERS)/api/gpio.c \
 	$(DRIVERS)/afe/ad4110/ad4110.c
 				
 SRCS += $(PLATFORM_DRIVERS)/xilinx_spi.c \

projects/ad463x_fmcz/src.mk
@@ -10,8 +10,9 @@
 ################################################################################
 
 SRCS := $(PROJECT)/src/ad463x_fmc.c
-SRCS += $(DRIVERS)/spi/spi.c \
-	$(DRIVERS)/gpio/gpio.c \
+SRCS += $(DRIVERS)/api/spi.c \
+	$(DRIVERS)/api/gpio.c \
+	$(DRIVERS)/api/irq.c \
 	$(DRIVERS)/adc/ad463x/ad463x.c \
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c \
 	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c \
@@ -27,7 +28,7 @@ LIBRARIES += iio
 SRC_DIRS += $(NO-OS)/iio/iio_app
 SRCS += $(PLATFORM_DRIVERS)/uart.c \
 	$(PLATFORM_DRIVERS)/$(PLATFORM)_irq.c \
-	$(DRIVERS)/irq/irq.c \
+	$(DRIVERS)/api/irq.c \
 	$(DRIVERS)/adc/ad463x/iio_ad463x.c \
 	$(NO-OS)/util/fifo.c \
 	$(NO-OS)/util/list.c

projects/ad5766-sdz/src.mk
@@ -12,8 +12,8 @@
 SRCS += $(PROJECT)/src/ad5766_sdz.c
 SRCS += $(PROJECT)/src/ad5766_core.c \
 	$(DRIVERS)/dac/ad5766/ad5766.c \
-	$(DRIVERS)/spi/spi.c \
-	$(DRIVERS)/gpio/gpio.c \
+	$(DRIVERS)/api/spi.c \
+	$(DRIVERS)/api/gpio.c \
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c \
 	$(DRIVERS)/axi_core/spi_engine/spi_engine.c \
 	$(NO-OS)/util/util.c

projects/ad6676-ebz/src.mk
@@ -21,8 +21,8 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c \
-	$(DRIVERS)/spi/spi.c \
-	$(DRIVERS)/gpio/gpio.c \
+	$(DRIVERS)/api/spi.c \
+	$(DRIVERS)/api/gpio.c \
 	$(DRIVERS)/adc/ad6676/ad6676.c \
 	$(NO-OS)/util/util.c
 SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c \
@@ -36,7 +36,7 @@ SRCS += $(NO-OS)/util/fifo.c \
 	$(DRIVERS)/axi_core/iio_axi_adc/iio_axi_adc.c \
 	$(PLATFORM_DRIVERS)/uart.c \
 	$(PLATFORM_DRIVERS)/$(PLATFORM)_irq.c \
-	$(DRIVERS)/irq/irq.c
+	$(DRIVERS)/api/irq.c
 endif
 INCS +=	$(PROJECT)/src/app_config.h \
 	$(PROJECT)/src/parameters.h

projects/ad7124-4sdz/src.mk
@@ -10,7 +10,7 @@
 ################################################################################
 
 SRCS += $(PROJECT)/src/ad7124-4sdz.c
-SRCS += $(DRIVERS)/spi/spi.c \
+SRCS += $(DRIVERS)/api/spi.c \
 	$(DRIVERS)/adc/ad7124/ad7124.c \
 	$(DRIVERS)/adc/ad7124/ad7124_regs.c				
 SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c \

projects/ad7124-8pmdz/src.mk
@@ -6,7 +6,8 @@ SRC_DIRS += $(NO-OS)/iio/iio_app
 # Add to SRCS source files to be build in the project
 SRCS += $(NO-OS)/drivers/adc/ad7124/ad7124.c \
 	$(NO-OS)/drivers/adc/ad7124/iio_ad7124.c \
-	$(NO-OS)/drivers/spi/spi.c
+	$(DRIVERS)/api/spi.c \
+	$(DRIVERS)/api/irq.c
 
 # Add to INCS inlcude files to be build in the porject
 INCS += $(NO-OS)/drivers/adc/ad7124/ad7124.h \
@@ -15,7 +16,6 @@ INCS += $(NO-OS)/drivers/adc/ad7124/ad7124.h \
 SRC_DIRS += $(PLATFORM_DRIVERS)
 SRC_DIRS += $(NO-OS)/util
 SRC_DIRS += $(INCLUDE)
-SRC_DIRS += $(NO-OS)/drivers/irq
 
 TINYIIOD=y
 

commit 1798273fddc030f640aebd57fa1d8dd74b67a266

projects:ad469x_fmcz:src.mk: Small fix
Fixes: 737663eab127 ("drivers:api:Platform ops restructured")

Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

projects/ad469x_fmcz/src.mk
@@ -27,7 +27,7 @@ LIBRARIES += iio
 SRC_DIRS += $(NO-OS)/iio/iio_app	
 SRCS += $(PLATFORM_DRIVERS)/uart.c \
 	$(PLATFORM_DRIVERS)/$(PLATFORM)_irq.c \
-	$(DRIVERS)/irq/irq.c \
+	$(DRIVERS)/api/irq.c \
 	$(NO-OS)/util/fifo.c \
 	$(NO-OS)/util/list.c						
 endif