CiprianRegus/no-OS-examples

commit 9e3ba7f923a4d44697614fd0feeac9834f1e9141

include:no_os_spi.h Add no_os_prefix to functions, enums, structures.
Added the "no_os_" prefix to functions declarations, enums, and
structs in no_os_spi.h and changed all connected files.

Signed-off-by: George Mois <george.mois@analog.com>

projects/eval-adxl355-pmdz/src/main.c
@@ -17,25 +17,24 @@
 #define SPI_CS			15
 #define SPI_CS_PORT		GPIOA
 
-extern UART_HandleTypeDef huart5;
-
 int main ()
 {
 	struct uart_desc *uart;
 	struct adxl355_dev *adxl355;
 
 	int ret;
-	stm32_init();
+	HAL_Init();
+	SystemClock_Config();
 
 	struct stm32_spi_init_param xsip  = {
 		.chip_select_port = SPI_CS_PORT,
 		.get_input_clock = HAL_RCC_GetPCLK1Freq,
 	};
 
-	struct spi_init_param sip = {
+	struct no_os_spi_init_param sip = {
 		.device_id = SPI_DEVICE_ID,
 		.max_speed_hz = 4000000,
-		.bit_order = SPI_BIT_ORDER_MSB_FIRST,
+		.bit_order = NO_OS_SPI_BIT_ORDER_MSB_FIRST,
 		.mode = NO_OS_SPI_MODE_0,
 		.extra = &xsip,
 		.platform_ops = &stm32_spi_ops,

projects/ad3552r_fmcz/srcs/main.c
@@ -178,7 +178,7 @@ int main()
 			.device_id = SPI_DEVICE_ID,
 			.chip_select = 0,
 			.mode = NO_OS_SPI_MODE_0,
-			.bit_order = SPI_BIT_ORDER_MSB_FIRST,
+			.bit_order = NO_OS_SPI_BIT_ORDER_MSB_FIRST,
 			.platform_ops = &xil_spi_reg_ops_pl,
 			.extra = NULL
 		},

projects/ad4110/src/app/main.c
@@ -67,12 +67,12 @@ int main()
 		.type = SPI_PS,
 		.flags = 0U
 	};
-	struct spi_init_param spi_ip = {
+	struct no_os_spi_init_param spi_ip = {
 		.device_id = SPI_DEVICE_ID,
 		.max_speed_hz = 100000,
 		.mode = NO_OS_SPI_MODE_0,
 		.chip_select = 0U,
-		.bit_order = SPI_BIT_ORDER_MSB_FIRST,
+		.bit_order = NO_OS_SPI_BIT_ORDER_MSB_FIRST,
 		.platform_ops = &xil_spi_ops,
 		.extra = &spi_extra
 	};

projects/ad7124-8pmdz/src/main.c
@@ -97,7 +97,7 @@ int main(void)
 		.half_duplex = false,
 		.master_mode = MASTER
 	};
-	struct spi_init_param spi_initial = {
+	struct no_os_spi_init_param spi_initial = {
 		.chip_select = 0x00,
 		.max_speed_hz = 10000000,
 		.mode = NO_OS_SPI_MODE_3,

projects/ad9172/src/main.c
@@ -74,7 +74,7 @@ int main(void)
 		.flags = 0
 	};
 
-	struct spi_init_param hmc7044_spi_param = {
+	struct no_os_spi_init_param hmc7044_spi_param = {
 		.device_id = SPI_DEVICE_ID,
 		.max_speed_hz = 10000000,
 		.mode = NO_OS_SPI_MODE_0,
@@ -131,7 +131,7 @@ int main(void)
 		.ref_rate_khz = 368640,		/* FPGA_CLK, output 12 of HMC 7044 */
 	};
 
-	struct spi_init_param ad9172_spi_param = {
+	struct no_os_spi_init_param ad9172_spi_param = {
 		.max_speed_hz = 1000000,
 		.mode = NO_OS_SPI_MODE_0,
 		.chip_select = SPI_AD9172_CS,
@@ -144,7 +144,7 @@ int main(void)
 		.device_id = GPIO_DEVICE_ID
 	};
 	struct ad9172_init_param ad9172_param = {
-		.spi_init = &ad9172_spi_param,	/* spi_init_param */
+		.spi_init = &ad9172_spi_param,	/* no_os_spi_init_param */
 		.gpio_reset = {
 			.number = 54 + 0,
 			.platform_ops = &xil_gpio_ops,

projects/ad9208/src/main.c
@@ -132,7 +132,7 @@ int main(void)
 		.flags = 0
 	};
 
-	struct spi_init_param hmc7044_spi_param = {
+	struct no_os_spi_init_param hmc7044_spi_param = {
 		.device_id = SPI_DEVICE_ID,
 		.max_speed_hz = 10000000,
 		.mode = NO_OS_SPI_MODE_0,
@@ -207,7 +207,7 @@ int main(void)
 		.channels = chan_spec,
 	};
 
-	struct spi_init_param ad9208_0_spi_param = {
+	struct no_os_spi_init_param ad9208_0_spi_param = {
 		.max_speed_hz = 10000000,
 		.mode = NO_OS_SPI_MODE_3,
 		.chip_select = SPI_AD9208_0_CS,
@@ -215,7 +215,7 @@ int main(void)
 		.extra = &xil_spi_param
 	};
 
-	struct spi_init_param ad9208_1_spi_param = {
+	struct no_os_spi_init_param ad9208_1_spi_param = {
 		.max_speed_hz = 10000000,
 		.mode = NO_OS_SPI_MODE_3,
 		.chip_select = SPI_AD9208_CS,

projects/adxrs290-pmdz/src/main.c
@@ -81,7 +81,7 @@ int main(void)
 		.master_mode = MASTER
 	};
 
-	struct spi_init_param init_param = {
+	struct no_os_spi_init_param init_param = {
 		.device_id = 1,
 		.chip_select = 0,
 		.extra = &spi_param,

projects/display_demo/src/app/main.c
@@ -66,12 +66,12 @@ int main(void)
 		.flags = 0U
 	};
 
-	struct spi_init_param ssd_1306_spi_init_param = {
+	struct no_os_spi_init_param ssd_1306_spi_init_param = {
 		.device_id = SPI_DEVICE_ID,
 		.max_speed_hz = 10000000,
 		.mode = NO_OS_SPI_MODE_0,
 		.chip_select = 10U,
-		.bit_order = SPI_BIT_ORDER_MSB_FIRST,
+		.bit_order = NO_OS_SPI_BIT_ORDER_MSB_FIRST,
 		.platform_ops = &xil_spi_ops,
 		.extra = &spi_extra
 	};

commit e69f5cf849ce09b39c75f1d4e497ef22c3e2b7ca

projects:eval-adxl355-pmdz:src:main.c Fixes main project
Fixes main file with stm init and huart5 declaration which
were previously removed.

Fixes: 9e3ba7f (include:no_os_spi.h Add no_os_prefix to functions, enums, structures.)
Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>

projects/eval-adxl355-pmdz/src/main.c
@@ -17,14 +17,15 @@
 #define SPI_CS			15
 #define SPI_CS_PORT		GPIOA
 
+UART_HandleTypeDef huart5;
+
 int main ()
 {
 	struct no_os_uart_desc *uart;
 	struct adxl355_dev *adxl355;
 
 	int ret;
-	HAL_Init();
-	SystemClock_Config();
+	stm32_init();
 
 	struct stm32_spi_init_param xsip  = {
 		.chip_select_port = SPI_CS_PORT,