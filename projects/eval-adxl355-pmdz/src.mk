Kevin-Lwk/no-OS

projects:eval-adxl355-pmdz: add IIO support
Provide initial IIO support for eval-adxl355-pmdz project.

Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>

projects/eval-adxl355-pmdz/src.mk
@@ -1,8 +1,10 @@
 SRC_DIRS += $(PROJECT)/src
 
-SRCS +=	$(NO-OS)/util/util.c
+ifeq (y,$(strip $(TINYIIOD)))
+SRC_DIRS += $(NO-OS)/iio/iio_app
+endif
 
-INCS +=	$(INCLUDE)/no-os/uart.h \
+INCS += $(INCLUDE)/no-os/uart.h \
 	$(INCLUDE)/no-os/spi.h \
 	$(INCLUDE)/no-os/i2c.h \
 	$(INCLUDE)/no-os/util.h \
@@ -11,16 +13,33 @@ INCS +=	$(INCLUDE)/no-os/uart.h \
 	$(INCLUDE)/no-os/timer.h \
 	$(INCLUDE)/no-os/irq.h \
 	$(INCLUDE)/no-os/rtc.h \
-    $(INCLUDE)/no-os/util.h \
-    $(INCLUDE)/no-os/print_log.h \
+	$(INCLUDE)/no-os/print_log.h \
 	$(INCLUDE)/no-os/gpio.h
 
-SRCS += $(DRIVERS)/api/spi.c \
-	$(DRIVERS)/api/i2c.c \
-	$(DRIVERS)/api/gpio.c \
-	$(DRIVERS)/accel/adxl355/adxl355.c
+INCS += $(PLATFORM_DRIVERS)/stm32_uart_stdio.h \
+	$(PLATFORM_DRIVERS)/stm32_uart.h \
+	$(PLATFORM_DRIVERS)/stm32_delay.h \
+	$(PLATFORM_DRIVERS)/stm32_spi.h \
+	$(PLATFORM_DRIVERS)/stm32_gpio.h \
+	$(PLATFORM_DRIVERS)/stm32_hal.h
 
 INCS += $(DRIVERS)/accel/adxl355/adxl355.h
+
+ifeq (y,$(strip $(TINYIIOD)))
+INCS += $(DRIVERS)/accel/adxl355/iio_adxl355.h
+
+INCS += $(INCLUDE)/no-os/fifo.h \
+	$(INCLUDE)/no-os/list.h \
+	$(PLATFORM_DRIVERS)/uart_extra.h
+
+INCS += $(PROJECT)/src/app_config.h \
+	$(PROJECT)/src/parameters.h
+endif
+
+SRCS += $(NO-OS)/util/util.c \
+	$(DRIVERS)/api/spi.c \
+	$(DRIVERS)/api/i2c.c \
+	$(DRIVERS)/api/gpio.c
 
 SRCS += $(PLATFORM_DRIVERS)/stm32_delay.c \
 	$(PLATFORM_DRIVERS)/stm32_gpio.c \
@@ -28,9 +47,11 @@ SRCS += $(PLATFORM_DRIVERS)/stm32_delay.c \
 	$(PLATFORM_DRIVERS)/stm32_uart.c \
 	$(PLATFORM_DRIVERS)/stm32_uart_stdio.c
 
-INCS +=	$(INCLUDE)/no-os/delay.h \
-	$(PLATFORM_DRIVERS)/stm32_uart_stdio.h \
-	$(PLATFORM_DRIVERS)/stm32_uart.h \
-	$(PLATFORM_DRIVERS)/stm32_spi.h \
-	$(PLATFORM_DRIVERS)/stm32_gpio.h \
-	$(PLATFORM_DRIVERS)/stm32_hal.h
+SRCS += $(DRIVERS)/accel/adxl355/adxl355.c
+
+ifeq (y,$(strip $(TINYIIOD)))
+SRCS += $(DRIVERS)/accel/adxl355/iio_adxl355.c
+
+SRCS += $(NO-OS)/util/fifo.c \
+	$(NO-OS)/util/list.c
+endif

projects/eval-adxl355-pmdz/src/main.c
@@ -12,6 +12,12 @@
 #include "stm32_uart.h"
 #include "stm32_uart_stdio.h"
 #include "adxl355.h"
+#ifdef IIO_SUPPORT
+#include "app_config.h"
+#include "parameters.h"
+#include "iio_app.h"
+#include "iio_adxl355.h"
+#endif
 
 #define SPI_DEVICE_ID	 1
 #define SPI_CS			15
@@ -19,13 +25,15 @@
 
 int main ()
 {
+#ifdef IIO_SUPPORT
+	struct adxl355_iio_dev *adxl355_iio_desc;
+	struct adxl355_iio_init_param adxl355_init_par;
+#else
 	struct uart_desc *uart;
 	struct adxl355_dev *adxl355;
+#endif
 
 	int ret;
-	HAL_Init();
-	SystemClock_Config();
-
 	struct stm32_spi_init_param xsip  = {
 		.chip_select_port = SPI_CS_PORT,
 		.get_input_clock = HAL_RCC_GetPCLK1Freq,
@@ -46,6 +54,9 @@ int main ()
 		.comm_type = ADXL355_SPI_COMM,
 	};
 
+#ifdef IIO_SUPPORT
+	adxl355_init_par.adxl355_initial = &init_data_adxl355;
+#else
 	struct stm32_uart_init_param xuip = {
 		.mode = UART_MODE_TX_RX,
 		.timeout = 10,
@@ -58,7 +69,28 @@ int main ()
 		.stop = UART_STOP_1_BIT,
 		.extra = &xuip,
 	};
+#endif
 
+	HAL_Init();
+	SystemClock_Config();
+
+#ifdef IIO_SUPPORT
+	ret = adxl355_iio_init(&adxl355_iio_desc, &adxl355_init_par);
+	if (ret != SUCCESS)
+		return ret;
+
+	struct iio_app_device iio_devices[] = {
+		{
+			.name = "adxl355_demo",
+			.dev = adxl355_iio_desc,
+			.dev_descriptor = adxl355_iio_desc->iio_dev,
+			.read_buff = NULL,
+			.write_buff = NULL
+		}
+	};
+
+	return iio_app_run(iio_devices, ARRAY_SIZE(iio_devices));
+#else
 	ret = uart_init(&uart, &uip);
 	if (ret < 0)
 		goto error;
@@ -136,4 +168,6 @@ int main ()
 error:
 	pr_info("Error!\n");
 	return 0;
+#endif
 }
+

CiprianRegus/no-OS-examples

projects/eval-adxl355-pmdz/src.mk
@@ -1,6 +1,8 @@
 SRC_DIRS += $(PROJECT)/src
 
-SRCS +=	$(NO-OS)/util/no_os_util.c
+ifeq (y,$(strip $(TINYIIOD)))
+SRC_DIRS += $(NO-OS)/iio/iio_app
+endif
 
 INCS +=	$(INCLUDE)/no_os_uart.h \
 	$(INCLUDE)/no_os_spi.h \
@@ -15,12 +17,30 @@ INCS +=	$(INCLUDE)/no_os_uart.h \
     $(INCLUDE)/no_os_print_log.h \
 	$(INCLUDE)/no_os_gpio.h
 
-SRCS += $(DRIVERS)/api/no_os_spi.c \
-	$(DRIVERS)/api/no_os_i2c.c \
-	$(DRIVERS)/api/no_os_gpio.c \
-	$(DRIVERS)/accel/adxl355/adxl355.c
+INCS += $(PLATFORM_DRIVERS)/stm32_uart_stdio.h \
+	$(PLATFORM_DRIVERS)/stm32_uart.h \
+	$(PLATFORM_DRIVERS)/stm32_delay.h \
+	$(PLATFORM_DRIVERS)/stm32_spi.h \
+	$(PLATFORM_DRIVERS)/stm32_gpio.h \
+	$(PLATFORM_DRIVERS)/stm32_hal.h
 
 INCS += $(DRIVERS)/accel/adxl355/adxl355.h
+
+ifeq (y,$(strip $(TINYIIOD)))
+INCS += $(DRIVERS)/accel/adxl355/iio_adxl355.h
+
+INCS += $(INCLUDE)/no-os/fifo.h \
+	$(INCLUDE)/no_os_list.h \
+	$(PLATFORM_DRIVERS)/uart_extra.h
+
+INCS += $(PROJECT)/src/app_config.h \
+	$(PROJECT)/src/parameters.h
+endif
+
+SRCS += $(NO-OS)/util/no_os_util.c \
+	$(DRIVERS)/api/no_os_spi.c \
+	$(DRIVERS)/api/no_os_i2c.c \
+	$(DRIVERS)/api/no_os_gpio.c
 
 SRCS += $(PLATFORM_DRIVERS)/stm32_delay.c \
 	$(PLATFORM_DRIVERS)/stm32_gpio.c \
@@ -28,9 +48,9 @@ SRCS += $(PLATFORM_DRIVERS)/stm32_delay.c \
 	$(PLATFORM_DRIVERS)/stm32_uart.c \
 	$(PLATFORM_DRIVERS)/stm32_uart_stdio.c
 
-INCS +=	$(INCLUDE)/no_os_delay.h \
-	$(PLATFORM_DRIVERS)/stm32_uart_stdio.h \
-	$(PLATFORM_DRIVERS)/stm32_uart.h \
-	$(PLATFORM_DRIVERS)/stm32_spi.h \
-	$(PLATFORM_DRIVERS)/stm32_gpio.h \
-	$(PLATFORM_DRIVERS)/stm32_hal.h
+SRCS += $(DRIVERS)/accel/adxl355/adxl355.c
+
+ifeq (y,$(strip $(TINYIIOD)))
+SRCS += $(DRIVERS)/accel/adxl355/iio_adxl355.c
+
+endif

projects/eval-adxl355-pmdz/src/main.c
@@ -12,21 +12,41 @@
 #include "stm32_uart.h"
 #include "stm32_uart_stdio.h"
 #include "adxl355.h"
+#ifdef IIO_SUPPORT
+#include "app_config.h"
+#include "parameters.h"
+#include "iio_app.h"
+#include "iio_adxl355.h"
+#endif
 
 #define SPI_DEVICE_ID	 1
 #define SPI_CS			15
 #define SPI_CS_PORT		GPIOA
 
-UART_HandleTypeDef huart5;
+#ifndef IIO_SUPPORT
+extern UART_HandleTypeDef huart5;
+#else
+#define DATA_BUFFER_SIZE 400
+
+// For output data you will need DATA_BUFFER_SIZE*4*sizeof(int32_t)
+uint8_t iio_data_buffer[DATA_BUFFER_SIZE*4*sizeof(int)];
+#endif
 
 int main ()
 {
+#ifdef IIO_SUPPORT
+	struct adxl355_iio_dev *adxl355_iio_desc;
+	struct adxl355_iio_init_param adxl355_init_par;
+	struct iio_data_buffer accel_buff = {
+		.buff = (void *)iio_data_buffer,
+		.size = DATA_BUFFER_SIZE*4*sizeof(int)
+	};
+#else
 	struct no_os_uart_desc *uart;
 	struct adxl355_dev *adxl355;
+#endif
 
 	int ret;
-	stm32_init();
-
 	struct stm32_spi_init_param xsip  = {
 		.chip_select_port = SPI_CS_PORT,
 		.get_input_clock = HAL_RCC_GetPCLK1Freq,
@@ -47,6 +67,9 @@ int main ()
 		.comm_type = ADXL355_SPI_COMM,
 	};
 
+#ifdef IIO_SUPPORT
+	adxl355_init_par.adxl355_initial = &init_data_adxl355;
+#else
 	struct stm32_uart_init_param xuip = {
 		.huart = &huart5,
 	};
@@ -58,7 +81,26 @@ int main ()
 		.stop = NO_OS_UART_STOP_1_BIT,
 		.extra = &xuip,
 	};
+#endif
 
+	stm32_init();
+
+#ifdef IIO_SUPPORT
+	ret = adxl355_iio_init(&adxl355_iio_desc, &adxl355_init_par);
+	if (ret != SUCCESS)
+		return ret;
+
+	struct iio_app_device iio_devices[] = {
+		{
+			.name = "adxl355",
+			.dev = adxl355_iio_desc,
+			.dev_descriptor = adxl355_iio_desc->iio_dev,
+			.read_buff = &accel_buff,
+		}
+	};
+
+	return iio_app_run(iio_devices, NO_OS_ARRAY_SIZE(iio_devices));
+#else
 	ret = no_os_uart_init(&uart, &uip);
 	if (ret < 0)
 		goto error;
@@ -136,4 +178,5 @@ int main ()
 error:
 	pr_info("Error!\n");
 	return 0;
+#endif
 }