CiprianRegus/no-OS-examples

commit 74fe67fe36bee58b0869f62a34d220872cdebd52

platform: xilinx: timer: use platform ops
Use platform ops for the xilinx timer driver.

Update projects accordingly.

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

projects/adv7511/src/main.c
@@ -360,6 +360,7 @@ int main()
 	timer_init.id = XPAR_XSCUTIMER_0_DEVICE_ID;
 	timer_init.freq_hz = XPAR_CPU_CORTEXA9_CORE_CLOCK_FREQ_HZ / 2;
 	timer_init.ticks_count = timer_init.freq_hz / 1000;
+	timer.platform_ops = &xil_timer_ops;
 	timer_init.extra = &xil_timer_init;
 #else
 	xil_timer_init.active_tmr = 0;
@@ -367,6 +368,7 @@ int main()
 	timer_init.id = XPAR_AXI_TIMER_DEVICE_ID;
 	timer_init.freq_hz = XPAR_AXI_TIMER_CLOCK_FREQ_HZ;
 	timer_init.ticks_count = timer_init.freq_hz / 1000;
+	timer.platform_ops = &xil_timer_ops;
 	timer_init.extra = &xil_timer_init;
 #endif
 #if defined(_XPARAMETERS_PS_H_)

projects/ad7768-evb/src.mk
@@ -47,6 +47,7 @@ SRC_DIRS += $(DRIVERS)/axi_core/axi_adc_core
 SRCS += $(DRIVERS)/api/no_os_gpio.c \
         $(DRIVERS)/api/no_os_i2c.c  \
         $(DRIVERS)/api/no_os_irq.c  \
+	$(DRIVERS)/api/no_os_timer.c \
         $(DRIVERS)/api/no_os_spi.c
 
 ifeq (y,$(strip $(TINYIIOD)))

projects/adv7511/src.mk
@@ -22,6 +22,7 @@ SRCS += $(DRIVERS)/axi_core/axi_dmac/axi_dmac.c \
 	$(DRIVERS)/api/no_os_gpio.c \
 	$(DRIVERS)/api/no_os_spi.c \
 	$(DRIVERS)/api/no_os_irq.c \
+	$(DRIVERS)/api/no_os_timer.c \
 	$(NO-OS)/util/no_os_util.c \
 	$(NO-OS)/util/no_os_list.c
 SRCS +=	$(PLATFORM_DRIVERS)/xilinx_axi_io.c \
@@ -30,7 +31,7 @@ SRCS +=	$(PLATFORM_DRIVERS)/xilinx_axi_io.c \
 	$(PLATFORM_DRIVERS)/xilinx_delay.c \
 	$(PLATFORM_DRIVERS)/xilinx_i2c.c \
 	$(PLATFORM_DRIVERS)/xilinx_irq.c \
-	$(PLATFORM_DRIVERS)/no_os_timer.c
+	$(PLATFORM_DRIVERS)/xilinx_timer.c
 SRCS +=$(PROJECT)/TX/HAL/COMMON/tx_hal.c \
 	$(PROJECT)/TX/HAL/WIRED/wrd_hal.c \
 	$(PROJECT)/TX/HAL/WIRED/ADV7511/7511_hal.c \

projects/iio_demo/src/platform/xilinx/platform_src.mk
@@ -1,5 +1,6 @@
 SRCS += $(PLATFORM_DRIVERS)/$(PLATFORM)_delay.c \
-	$(DRIVERS)/api/no_os_irq.c
+	$(DRIVERS)/api/no_os_irq.c \
+	$(DRIVERS)/api/no_os_timer.c
 	
 ifeq (y,$(strip $(NETWORKING)))
 DISABLE_SECURE_SOCKET ?= y
@@ -7,7 +8,7 @@ SRC_DIRS += $(NO-OS)/network
 endif
 
 SRCS += $(NO-OS)/util/no_os_circular_buffer.c \
-	$(PLATFORM_DRIVERS)/no_os_timer.c
+	$(PLATFORM_DRIVERS)/xilinx_timer.c
 
 INCS += $(INCLUDE)/no_os_circular_buffer.h \
 	$(INCLUDE)/no_os_timer.h           \

commit c292c971a3a19feb7915ffd14e7a241893c0dcb5

projects: adv7511: fix platform ops init
Use proper timer init structure when assigning the xil_timer_ops
structure.

Fixes: 74fe67f ("platform: xilinx: timer: use platform ops")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

projects/adv7511/src/main.c
@@ -360,7 +360,7 @@ int main()
 	timer_init.id = XPAR_XSCUTIMER_0_DEVICE_ID;
 	timer_init.freq_hz = XPAR_CPU_CORTEXA9_CORE_CLOCK_FREQ_HZ / 2;
 	timer_init.ticks_count = timer_init.freq_hz / 1000;
-	timer.platform_ops = &xil_timer_ops;
+	timer_init.platform_ops = &xil_timer_ops;
 	timer_init.extra = &xil_timer_init;
 #else
 	xil_timer_init.active_tmr = 0;
@@ -368,7 +368,7 @@ int main()
 	timer_init.id = XPAR_AXI_TIMER_DEVICE_ID;
 	timer_init.freq_hz = XPAR_AXI_TIMER_CLOCK_FREQ_HZ;
 	timer_init.ticks_count = timer_init.freq_hz / 1000;
-	timer.platform_ops = &xil_timer_ops;
+	timer_init.platform_ops = &xil_timer_ops;
 	timer_init.extra = &xil_timer_init;
 #endif
 #if defined(_XPARAMETERS_PS_H_)