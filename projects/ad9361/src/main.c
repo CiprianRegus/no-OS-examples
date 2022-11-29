CiprianRegus/no-OS-examples

commit 4749d4e63f752bd9a38d840f6913c5e1dacc984c

drivers:axi_core:axi_dmac:axi_dmac.* DMAC driver
Update of the DMAC driver:
- Separate ISRs for the three transfer types (DEV2MEM,
MEM2DEV, MEM2MEM);
- axi_dmac_detect_caps - function detecting DMAC
capabilities;
- flags for transfer are set depending on transfer size
and DMAC capabilities.

Signed-off-by: George Mois <george.mois@analog.com>

projects/ad9361/src/main.c
@@ -86,6 +86,10 @@ static uint8_t out_buff[MAX_SIZE_BASE_ADDR];
 
 #endif // IIO_SUPPORT
 
+#ifdef DAC_DMA_EXAMPLE
+#include <string.h>
+#endif
+
 /******************************************************************************/
 /************************ Variables Definitions *******************************/
 /******************************************************************************/
@@ -152,15 +156,21 @@ struct axi_dac_init tx_dac_init = {
 struct axi_dmac_init rx_dmac_init = {
 	"rx_dmac",
 	CF_AD9361_RX_DMA_BASEADDR,
-	DMA_DEV_TO_MEM,
-	0
+#ifdef ADC_DMA_IRQ_EXAMPLE
+	IRQ_ENABLED
+#else
+	IRQ_DISABLED
+#endif
 };
 struct axi_dmac *rx_dmac;
 struct axi_dmac_init tx_dmac_init = {
 	"tx_dmac",
 	CF_AD9361_TX_DMA_BASEADDR,
-	DMA_MEM_TO_DEV,
-	DMA_CYCLIC
+#ifdef ADC_DMA_IRQ_EXAMPLE
+	IRQ_ENABLED
+#else
+	IRQ_DISABLED
+#endif
 };
 struct axi_dmac *tx_dmac;
 
@@ -677,8 +687,8 @@ int main(void)
 	samples = 2097150;
 #endif
 	// NOTE: To prevent unwanted data loss, it's recommended to invalidate
-	// cache after each axi_dmac_transfer() call, keeping in mind that the
-	// size of the capture and the start address must be alinged to the size
+	// cache after each axi_dmac_transfer_start() call, keeping in mind that the
+	// size of the capture and the start address must be aligned to the size
 	// of the cache line.
 
 #ifdef DAC_DMA_EXAMPLE
@@ -699,21 +709,96 @@ int main(void)
 #endif
 
 #ifdef FMCOMMS5
-	axi_dmac_transfer_nonblocking(tx_dmac, DAC_DDR_BASEADDR,
-				      samples * AD9361_ADC_DAC_BYTES_PER_SAMPLE *
-				      (ad9361_phy_b->tx_dac->num_channels + ad9361_phy->tx_dac->num_channels));
+	struct axi_dma_transfer transfer = {
+		// Number of bytes to write/read
+		.size = samples * AD9361_ADC_DAC_BYTES_PER_SAMPLE *
+		(ad9361_phy_b->tx_dac->num_channels + ad9361_phy->tx_dac->num_channels),
+		// Transfer done flag
+		.transfer_done = 0,
+		// Signal transfer mode
+		.cyclic = CYCLIC,
+		// Address of data source
+		.src_addr = (uintptr_t)DAC_DDR_BASEADDR,
+		// Address of data destination
+		.dest_addr = 0
+	};
+
+	/* Transfer the data. */
+	axi_dmac_transfer_start(tx_dmac, &transfer);
+
+	/* Flush cache data. */
+	Xil_DCacheInvalidateRange((uintptr_t)DAC_DDR_BASEADDR,
+				  samples * AD9361_ADC_DAC_BYTES_PER_SAMPLE *
+				  (ad9361_phy_b->tx_dac->num_channels + ad9361_phy->tx_dac->num_channels));
+
+	no_os_mdelay(1000);
+
 #else
-	axi_dmac_transfer_nonblocking(tx_dmac, (uintptr_t)dac_buffer,
-				      sizeof(sine_lut_iq));
+	struct axi_dma_transfer transfer = {
+		// Number of bytes to write/read
+		.size = sizeof(sine_lut_iq),
+		// Transfer done flag
+		.transfer_done = 0,
+		// Signal transfer mode
+		.cyclic = CYCLIC,
+		// Address of data source
+		.src_addr = (uintptr_t)dac_buffer,
+		// Address of data destination
+		.dest_addr = 0
+	};
+
+	/* Transfer the data. */
+	axi_dmac_transfer_start(tx_dmac, &transfer);
+
+	/* Flush cache data. */
+	Xil_DCacheInvalidateRange((uintptr_t)dac_buffer,sizeof(sine_lut_iq));
+
 	no_os_mdelay(1000);
 #endif
 #endif
 #ifdef FMCOMMS5
-	axi_dmac_transfer(rx_dmac, ADC_DDR_BASEADDR,
-			  samples * AD9361_ADC_DAC_BYTES_PER_SAMPLE *
-			  (ad9361_phy_b->rx_adc->num_channels + ad9361_phy->rx_adc->num_channels));
+	struct axi_dma_transfer read_transfer = {
+		// Number of bytes to write/read
+		.size = samples * AD9361_ADC_DAC_BYTES_PER_SAMPLE *
+		(ad9361_phy_b->rx_adc->num_channels + ad9361_phy->rx_adc->num_channels),
+		// Transfer done flag
+		.transfer_done = 0,
+		// Signal transfer mode
+		.cyclic = NO,
+		// Address of data source
+		.src_addr = 0,
+		// Address of data destination
+		.dest_addr = (uintptr_t)ADC_DDR_BASEADDR
+	};
+
+	/* Read the data from the ADC DMA. */
+	axi_dmac_transfer_start(rx_dmac, &read_transfer);
+
+	/* Wait until transfer finishes */
+	status = axi_dmac_transfer_wait_completion(rx_dmac, 500);
+	if(status < 0)
+		return status;
 #else
-	axi_dmac_transfer(rx_dmac, (uintptr_t)adc_buffer, sizeof(adc_buffer));
+	struct axi_dma_transfer read_transfer = {
+		// Number of bytes to write/read
+		.size = sizeof(adc_buffer),
+		// Transfer done flag
+		.transfer_done = 0,
+		// Signal transfer mode
+		.cyclic = NO,
+		// Address of data source
+		.src_addr = 0,
+		// Address of data destination
+		.dest_addr = (uintptr_t)adc_buffer
+	};
+
+	/* Read the data from the ADC DMA. */
+	axi_dmac_transfer_start(rx_dmac, &read_transfer);
+
+	/* Wait until transfer finishes */
+	status = axi_dmac_transfer_wait_completion(rx_dmac, 500);
+	if(status < 0)
+		return status;
 #endif
 #ifdef XILINX_PLATFORM
 #ifdef FMCOMMS5

projects/ad9172/src/main.c
@@ -182,8 +182,7 @@ int main(void)
 	struct axi_dmac_init tx_dmac_init = {
 		"tx_dmac",
 		TX_DMA_BASEADDR,
-		DMA_MEM_TO_DEV,
-		DMA_LAST,
+		IRQ_DISABLED
 	};
 	struct axi_dmac *tx_dmac;
 #endif /* DAC_DMA_EXAMPLE */
@@ -259,8 +258,26 @@ int main(void)
 	Xil_DCacheFlush();
 #endif
 	axi_dmac_init(&tx_dmac, &tx_dmac_init);
-	axi_dmac_transfer(tx_dmac, DDR_MEM_BASEADDR,
-			  sizeof(sine_lut_iq) * (tx_dac->num_channels / 2));
+	struct axi_dma_transfer transfer = {
+		// Number of bytes to write/read
+		.size = sizeof(sine_lut_iq) * (tx_dac->num_channels / 2),
+		// Transfer done flag
+		.transfer_done = 0,
+		// Signal transfer mode
+		.cyclic = NO,
+		// Address of data source
+		.src_addr = (uintptr_t)DDR_MEM_BASEADDR,
+		// Address of data destination
+		.dest_addr = 0
+	};
+	axi_dmac_transfer_start(tx_dmac, &transfer);
+	/* Wait until transfer finishes */
+	status = axi_dmac_transfer_wait_completion(tx_dmac, 500);
+	if(status)
+		return status;
+	/* Flush cache data. */
+	Xil_DCacheInvalidateRange((uintptr_t)DDR_MEM_BASEADDR,
+				  sizeof(sine_lut_iq) * (tx_dac->num_channels / 2));
 #else /* DAC_DMA_EXAMPLE */
 	printf("Set dds frequency at 40MHz\n");
 
@@ -307,8 +324,7 @@ int main(void)
 	struct axi_dmac_init tx_dmac_init = {
 		"tx_dmac",
 		TX_DMA_BASEADDR,
-		DMA_MEM_TO_DEV,
-		DMA_LAST,
+		IRQ_DISABLED
 	};
 	struct axi_dmac *tx_dmac;
 

projects/ad9208/src/main.c
@@ -320,8 +320,7 @@ int main(void)
 	struct axi_dmac_init rx_dmac_init = {
 		.name = "rx_dmac",
 		.base = RX_DMA_BASEADDR,
-		.direction = DMA_DEV_TO_MEM,
-		.flags = 0,
+		.irq_option = IRQ_DISABLED
 	};
 
 	struct xil_gpio_init_param xilinx_gpio_init_param = {
@@ -555,10 +554,26 @@ int main(void)
 	}
 
 	size = (rx_1_adc->num_channels + rx_0_adc->num_channels) * sizeof(uint16_t);
-	status = axi_dmac_transfer(rx_dmac,
-				   ADC_DDR_BASEADDR, 16384 * size);
+	struct axi_dma_transfer read_transfer = {
+		// Number of bytes to write/read
+		.size = 16384 * size,
+		// Transfer done flag
+		.transfer_done = 0,
+		// Signal transfer mode
+		.cyclic = NO,
+		// Address of data source
+		.src_addr = 0,
+		// Address of data destination
+		.dest_addr = (uintptr_t)ADC_DDR_BASEADDR
+	};
+	status = axi_dmac_transfer_start(rx_dmac, &read_transfer);
 	if (status != 0)
-		xil_printf("axi_dmac_transfer() error: %"PRIi32"\n", status);
+		xil_printf("axi_dmac_transfer_start() error: %"PRIi32"\n", status);
+	/* Wait until transfer finishes */
+	status = axi_dmac_transfer_wait_completion(rx_dmac, 500);
+	if(status)
+		return status;
+	Xil_DCacheInvalidateRange((uintptr_t)ADC_DDR_BASEADDR, 16384 * size);
 
 #ifdef IIO_SUPPORT
 	status = start_iiod(rx_0_adc, rx_1_adc, rx_dmac);

projects/ad6676-ebz/src/ad6676_ebz.c
@@ -323,8 +323,7 @@ int main(void)
 	struct axi_dmac_init ad6676_dmac_param = {
 		.name = "ad6676_dmac",
 		.base = RX_DMA_BASEADDR,
-		.direction = DMA_DEV_TO_MEM,
-		.flags = 0
+		.irq_option = IRQ_DISABLED
 	};
 	struct axi_dmac *ad6676_dmac;
 
@@ -428,11 +427,49 @@ int main(void)
 
 	// test the captured data
 	axi_dmac_init(&ad6676_dmac, &ad6676_dmac_param);
-	axi_dmac_transfer(ad6676_dmac, ADC_DDR_BASEADDR, 16384 * 2);
+
+	struct axi_dma_transfer transfer_test = {
+		// Number of bytes to writen/read
+		.size = 16384 * 2,
+		// Transfer done flag
+		.transfer_done = 0,
+		// Signal transfer mode
+		.cyclic = NO,
+		// Address of data source
+		.src_addr = 0,
+		// Address of data destination
+		.dest_addr = (uintptr_t)ADC_DDR_BASEADDR
+	};
+	axi_dmac_transfer_start(ad6676_dmac, &transfer_test);
+	/* Wait until transfer finishes */
+	int32_t status = axi_dmac_transfer_wait_completion(ad6676_dmac, 500);
+	if(status)
+		return status;
+	/* Flush cache data. */
+	Xil_DCacheInvalidateRange((uintptr_t)ADC_DDR_BASEADDR,16384 * 2);
 
 	// capture data with DMA
 	ad6676_test(ad6676_device, TESTGENMODE_OFF);
-	axi_dmac_transfer(ad6676_dmac, ADC_DDR_BASEADDR, 16384 * 2);
+
+	struct axi_dma_transfer transfer_capture = {
+		// Number of bytes to writen/read
+		.size = 16384 * 2,
+		// Transfer done flag
+		.transfer_done = 0,
+		// Signal transfer mode
+		.cyclic = NO,
+		// Address of data source
+		.src_addr = 0,
+		// Address of data destination
+		.dest_addr = (uintptr_t)ADC_DDR_BASEADDR
+	};
+	axi_dmac_transfer_start(ad6676_dmac, &transfer_capture);
+	/* Wait until transfer finishes */
+	status = axi_dmac_transfer_wait_completion(ad6676_dmac, 500);
+	if(status)
+		return status;
+	/* Flush cache data. */
+	Xil_DCacheInvalidateRange((uintptr_t)ADC_DDR_BASEADDR,16384 * 2);
 
 #ifdef IIO_SUPPORT
 

projects/ad7768-evb/src/ad7768_evb.c
@@ -147,10 +147,9 @@ int main(void)
 	ad7768_dev	*dev;
 	struct xil_gpio_init_param axi_gpio_init;
 	struct axi_dmac_init dma_initial = {
+		.name = "ad7768_dma",
 		.base = AD7768_DMA_BASEADDR,
-		.direction = DMA_DEV_TO_MEM,
-		.flags = 0,
-		.name = "ad7768_dma"
+		.irq_option = IRQ_DISABLED
 	};
 	struct axi_dmac *dma_desc;
 	int32_t ret;
@@ -221,6 +220,11 @@ int main(void)
 	};
 	int32_t *data_ptr, i;
 
+	/* Enable the instruction cache. */
+	Xil_ICacheEnable();
+	/* Enable the data cache. */
+	Xil_DCacheEnable();
+
 	ret = ad7768_if_gpio_setup(GPIO_UP_SSHOT, NO_OS_GPIO_LOW);
 	if (ret != 0)
 		return ret;
@@ -268,9 +272,25 @@ int main(void)
 		     ((resolution + AD7768_HEADER_SIZE) / BITS_IN_BYTE));
 
 	printf("Capture samples...\n");
-	ret = axi_dmac_transfer(dma_desc, ADC_DDR_BASEADDR, data_size);
+	struct axi_dma_transfer read_transfer = {
+		// Number of bytes to write/read
+		.size = data_size,
+		// Transfer done flag
+		.transfer_done = 0,
+		// Signal transfer mode
+		.cyclic = NO,
+		// Address of data source
+		.src_addr = 0,
+		// Address of data destination
+		.dest_addr = (uintptr_t)ADC_DDR_BASEADDR
+	};
+	ret = axi_dmac_transfer_start(dma_desc, &read_transfer);
 	if (ret != 0)
 		return -1;
+	ret = axi_dmac_transfer_wait_completion(dma_desc, 500);
+	if (ret)
+		return ret;
+	Xil_DCacheInvalidateRange((uintptr_t)ADC_DDR_BASEADDR,data_size);
 	printf("Capture done\n");
 
 	if (ad7768_evb_verify_status(&axi_gpio_init))
@@ -316,6 +336,11 @@ int main(void)
 
 	return iio_app_run(devs, NO_OS_ARRAY_SIZE(devs));
 #endif
+
+	/* Disable the instruction cache. */
+	Xil_DCacheDisable();
+	/* Disable the data cache. */
+	Xil_ICacheDisable();
 
 	return 0;
 }

projects/ad9081/src/app.c
@@ -200,15 +200,13 @@ int main(void)
 	struct axi_dmac_init rx_dmac_init = {
 		"rx_dmac",
 		RX_DMA_BASEADDR,
-		DMA_DEV_TO_MEM,
-		0
+		IRQ_DISABLED
 	};
 	struct axi_dmac *rx_dmac;
 	struct axi_dmac_init tx_dmac_init = {
 		"tx_dmac",
 		TX_DMA_BASEADDR,
-		DMA_MEM_TO_DEV,
-		DMA_CYCLIC
+		IRQ_DISABLED
 	};
 	struct axi_dmac *tx_dmac;
 	struct ad9081_phy* phy[MULTIDEVICE_INSTANCE_COUNT];

projects/ad9083/src/app.c
@@ -89,8 +89,7 @@ int main(void)
 	struct axi_dmac_init rx_dmac_init = {
 		"rx_dmac",
 		RX_DMA_BASEADDR,
-		DMA_DEV_TO_MEM,
-		0
+		IRQ_DISABLED
 	};
 
 	struct app_clocking_init app_clocking_init_param = {

projects/ad9265-fmc-125ebz/src/ad9265_fmc_125ebz.c
@@ -91,8 +91,7 @@ int main(void)
 	struct axi_dmac_init ad9265_dmac_param = {
 		.name = "ad9265_dmac",
 		.base = RX_DMA_BASEADDR,
-		.direction = DMA_DEV_TO_MEM,
-		.flags = 0
+		.irq_option = IRQ_DISABLED
 	};
 	struct axi_dmac *ad9265_dmac;
 
@@ -133,11 +132,28 @@ int main(void)
 		return -1;
 	}
 
-	status = axi_dmac_transfer(ad9265_dmac, ADC_DDR_BASEADDR, 16384 * 2);
+	struct axi_dma_transfer read_transfer = {
+		// Number of bytes to write/read
+		.size = 16384 * 2,
+		// Transfer done flag
+		.transfer_done = 0,
+		// Signal transfer mode
+		.cyclic = NO,
+		// Address of data source
+		.src_addr = 0,
+		// Address of data destination
+		.dest_addr = (uintptr_t)ADC_DDR_BASEADDR
+	};
+	status = axi_dmac_transfer_start(ad9265_dmac, &read_transfer);
 	if (status != 0) {
-		pr_err("axi_dmac_transfer() failed!\n");
+		pr_err("axi_dmac_transfer_start() failed!\n");
 		return -1;
 	}
+	/* Wait until transfer finishes */
+	status = axi_dmac_transfer_wait_completion(ad9265_dmac, 500);
+	if(status < 0)
+		return status;
+	Xil_DCacheInvalidateRange((uintptr_t)ADC_DDR_BASEADDR, 16384 * 2);
 
 	pr_info("Capture done.\n");
 

commit b24706a75580b2f7cfc349622258c3fe0614d3ed

ad9361:src:main.c FMCOMMS5 DMA example fix
ADC and DAC DMA transfers example, with IRQ_ENABLED.

Fixes: 4749d4e (drivers:axi_core:axi_dmac:axi_dmac.* DMAC driver)
Signed-off-by: George Mois <george.mois@analog.com>

projects/ad9361/src/main.c
@@ -607,8 +607,11 @@ int main(void)
 #if defined XILINX_PLATFORM || defined LINUX_PLATFORM || defined ALTERA_PLATFORM
 #ifdef DAC_DMA_EXAMPLE
 #ifdef FMCOMMS5
-	axi_dac_init(&ad9361_phy->tx_dac, &tx_dac_init);
+	axi_dac_init(&ad9361_phy_b->tx_dac, &tx_dac_init);
+	axi_adc_init(&ad9361_phy_b->rx_adc, &rx_adc_init);
 	axi_dac_set_datasel(ad9361_phy_b->tx_dac, -1, AXI_DAC_DATA_SEL_DMA);
+	rx_adc_init.base = AD9361_RX_0_BASEADDR;
+	tx_dac_init.base = AD9361_TX_0_BASEADDR;
 #endif
 	axi_dac_init(&ad9361_phy->tx_dac, &tx_dac_init);
 	axi_adc_init(&ad9361_phy->rx_adc, &rx_adc_init);
@@ -688,7 +691,7 @@ int main(void)
 	if(status < 0)
 		return status;
 
-	samples = 2097150;
+	samples = 2048;
 #endif
 	// NOTE: To prevent unwanted data loss, it's recommended to invalidate
 	// cache after each axi_dmac_transfer_start() call, keeping in mind that the
@@ -715,14 +718,13 @@ int main(void)
 #ifdef FMCOMMS5
 	struct axi_dma_transfer transfer = {
 		// Number of bytes to write/read
-		.size = samples * AD9361_ADC_DAC_BYTES_PER_SAMPLE *
-		(ad9361_phy_b->tx_dac->num_channels + ad9361_phy->tx_dac->num_channels),
+		.size = sizeof(sine_lut_iq),
 		// Transfer done flag
 		.transfer_done = 0,
 		// Signal transfer mode
 		.cyclic = CYCLIC,
 		// Address of data source
-		.src_addr = (uintptr_t)DAC_DDR_BASEADDR,
+		.src_addr = (uintptr_t)dac_buffer,
 		// Address of data destination
 		.dest_addr = 0
 	};
@@ -731,9 +733,7 @@ int main(void)
 	axi_dmac_transfer_start(tx_dmac, &transfer);
 
 	/* Flush cache data. */
-	Xil_DCacheInvalidateRange((uintptr_t)DAC_DDR_BASEADDR,
-				  samples * AD9361_ADC_DAC_BYTES_PER_SAMPLE *
-				  (ad9361_phy_b->tx_dac->num_channels + ad9361_phy->tx_dac->num_channels));
+	Xil_DCacheInvalidateRange((uintptr_t)dac_buffer, sizeof(sine_lut_iq));
 
 	no_os_mdelay(1000);
 
@@ -806,16 +806,21 @@ int main(void)
 #endif
 #ifdef XILINX_PLATFORM
 #ifdef FMCOMMS5
-	Xil_DCacheInvalidateRange(ADC_DDR_BASEADDR,
+	Xil_DCacheInvalidateRange((uintptr_t)ADC_DDR_BASEADDR,
 				  samples * AD9361_ADC_DAC_BYTES_PER_SAMPLE * (ad9361_phy_b->rx_adc->num_channels
 						  +
 						  ad9361_phy->rx_adc->num_channels));
+	printf("DAC_DMA_EXAMPLE: address=%#x samples=%lu channels=%u bits=%u\n",
+	       (uintptr_t)ADC_DDR_BASEADDR,
+	       read_transfer.size / AD9361_ADC_DAC_BYTES_PER_SAMPLE,
+	       rx_adc_init.num_channels * 2,
+	       8 * sizeof(adc_buffer[0]));
 #else
 	Xil_DCacheInvalidateRange((uintptr_t)adc_buffer, sizeof(adc_buffer));
-#endif
 	printf("DAC_DMA_EXAMPLE: address=%#lx samples=%lu channels=%u bits=%lu\n",
 	       (uintptr_t)adc_buffer, NO_OS_ARRAY_SIZE(adc_buffer), rx_adc_init.num_channels,
 	       8 * sizeof(adc_buffer[0]));
+#endif
 #endif
 #endif
 #endif