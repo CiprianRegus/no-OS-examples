CiprianRegus/no-OS-examples

commit 8fb9e0b08d254cfd3745c3e2c0a152120eb886c0

drivers:platform:maxim: Allow I2C vddio selection
Add the vssel field, so that the VDDIO may be configured for each I2C
interface. Adapt the projects accordingly.

Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

projects/adt7420-pmdz/src/platform/maxim/parameters.c
@@ -49,4 +49,8 @@
 struct max_uart_init_param xuip = {
 	.flow = UART_FLOW_DIS,
 };
+
+struct max_i2c_init_param adt7420_i2c_extra = {
+	.vssel = MXC_GPIO_VSSEL_VDDIOH;
+};
 #endif

projects/adt7420-pmdz/src/common/common_data.c
@@ -62,7 +62,8 @@ const struct no_os_i2c_init_param iip = {
 	.max_speed_hz = 100000,
 	.slave_address = 0x4B,
 	.extra = NULL,
-	.platform_ops = I2C_OPS
+	.platform_ops = I2C_OPS,
+	.extra = &adt7420_i2c_extra,
 };
 
 struct adt7420_init_param adt7420_user_init = {

projects/adt7420-pmdz/src/platform/maxim/parameters.h
@@ -65,5 +65,6 @@
 #define I2C_OPS         &max_i2c_ops
 
 extern struct max_uart_init_param xuip;
+extern struct max_i2c_init_param adt7420_i2c_extra;
 
 #endif /* __PARAMETERS_H__ */

commit 69305fc1e32393bb7af465e9165752e109130f20

adt7420-pmdz: Declare i2c_extra for all examples
Declare the adt7420_i2c_extra for both the DUMMY and IIO examples,
since it's used in both cases.

Fixes: 8fb9e0b ("drivers:platform:maxim: Allow I2C vddio selection")
Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

projects/adt7420-pmdz/src/platform/maxim/parameters.c
@@ -49,8 +49,8 @@
 struct max_uart_init_param xuip = {
 	.flow = UART_FLOW_DIS,
 };
+#endif
 
 struct max_i2c_init_param adt7420_i2c_extra = {
-	.vssel = MXC_GPIO_VSSEL_VDDIOH;
+	.vssel = MXC_GPIO_VSSEL_VDDIOH
 };
-#endif