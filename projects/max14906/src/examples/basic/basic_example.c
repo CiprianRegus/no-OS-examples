CiprianRegus/no-OS-examples

commit e884e28effca09851452fe64ba232dec57d96a02

projects: max14906 : Change interrupt support for MAXIM platform
Move specific interrupt support related function calls in
maxim main source file.
Add error labels in maxim main source file to handle the errors
that may appear from the new function calls.

Signed-off-by: Radu Sabau <radu.sabau@analog.com>

projects/max14906/src/examples/basic/basic_example.c
@@ -71,8 +71,6 @@ int basic_example_main()
 	enum max14906_climit climit = MAX14906_CL_300;
 	enum max14906_climit climit2 = MAX14906_CL_600;
 
-	struct no_os_gpio_desc *gpio_desc;
-
 	/** MAX14906 Initialization */
 	ret = max14906_init(&max14906_desc, &max14906_ip);
 	if (ret)
@@ -82,23 +80,15 @@ int basic_example_main()
 	struct no_os_irq_ctrl_desc *global_desc;
 	struct no_os_irq_init_param global_desc_param = {
 		.irq_ctrl_id = GPIO_IRQ_ID,
-		.platform_ops = &max_irq_ops,
-		.extra = NULL
+		.platform_ops = GPIO_IRQ_OPS,
+		.extra = GPIO_IRQ_EXTRA
 	};
 
 	ret = no_os_irq_ctrl_init(&global_desc, &global_desc_param);
 	if (ret)
 		goto max14906_remove;
 
-	/** GPIO Interrupt Controller */
-	struct no_os_irq_ctrl_desc *gpio_irq_desc;
-	struct no_os_irq_init_param gpio_irq_desc_param = {
-		.irq_ctrl_id = GPIO_IRQ_ID,
-		.platform_ops = GPIO_IRQ_OPS,
-		.extra = NULL
-	};
-
-	ret = no_os_irq_ctrl_init(&gpio_irq_desc, &gpio_irq_desc_param);
+	ret = no_os_irq_set_priority(global_desc, GPIO_IRQ_ID, 1);
 	if (ret)
 		goto remove_gpio_irq_ctrl;
 
@@ -111,25 +101,17 @@ int basic_example_main()
 		.handle = NULL
 	};
 
-	ret = no_os_irq_register_callback(gpio_irq_desc, GPIO_FAULT_PIN_NUM,
+	ret = no_os_irq_register_callback(global_desc, GPIO_IRQ_ID,
 					  &gpio_cb);
 	if (ret)
-		goto gpio_irq_remove;
+		goto remove_gpio_irq_ctrl;
 
-	ret = no_os_irq_trigger_level_set(gpio_irq_desc, GPIO_FAULT_PIN_NUM,
-					  NO_OS_IRQ_EDGE_BOTH);
+	ret = no_os_irq_trigger_level_set(global_desc, GPIO_IRQ_ID,
+					  NO_OS_IRQ_EDGE_FALLING);
 	if (ret)
 		goto gpio_irq_unregister_callback;
 
-	ret = no_os_irq_set_priority(gpio_irq_desc, GPIO_FAULT_PIN_NUM, 1);
-	if (ret)
-		goto gpio_irq_unregister_callback;
-
-	ret = no_os_irq_enable(gpio_irq_desc, GPIO_FAULT_PIN_NUM);
-	if (ret)
-		goto gpio_irq_unregister_callback;
-
-	ret = no_os_irq_enable(global_desc, GPIO0_IRQn);
+	ret = no_os_irq_enable(global_desc, GPIO_IRQ_ID);
 	if (ret)
 		goto gpio_irq_unregister_callback;
 
@@ -177,12 +159,14 @@ int basic_example_main()
 			goto gpio_irq_unregister_callback;
 
 		ret = max14906_ch_get(max14906_desc, 0, &val);
+		if (ret)
+			goto gpio_irq_unregister_callback;
 	}
 
+	while (1);
+
 gpio_irq_unregister_callback:
-	no_os_irq_unregister_callback(gpio_irq_desc, GPIO_FAULT_PIN_NUM, &gpio_cb);
-gpio_irq_remove:
-	no_os_irq_ctrl_remove(gpio_irq_desc);
+	no_os_irq_unregister_callback(global_desc, GPIO_FAULT_PIN_NUM, &gpio_cb);
 remove_gpio_irq_ctrl:
 	no_os_irq_ctrl_remove(global_desc);
 max14906_remove:

projects/max14906/src/platform/maxim/main.c
@@ -58,16 +58,34 @@ int main()
 
 #ifdef BASIC_EXAMPLE
 	struct no_os_uart_desc *uart_desc;
+	struct no_os_irq_ctrl_desc *gpio_irq_desc;
+	struct no_os_irq_init_param gpio_irq_desc_param = {
+		.irq_ctrl_id = GPIO_IRQ_ID,
+		.platform_ops = GPIO_IRQ_OPS,
+		.extra = NULL
+	};
 
 	ret = no_os_uart_init(&uart_desc, &max14906_uart_ip);
 	if (ret)
 		return ret;
 
+	ret = no_os_irq_ctrl_init(&gpio_irq_desc, &gpio_irq_desc_param);
+	if (ret)
+		goto max14906_uart_remove;
+
+	ret = no_os_irq_enable(gpio_irq_desc, GPIO0_IRQn);
+	if (ret)
+		goto max14906_irq_ctrl_remove;
+
 	no_os_uart_stdio(uart_desc);
 
 	ret = basic_example_main();
 
+max14906_irq_ctrl_remove:
+	no_os_irq_ctrl_remove(gpio_irq_desc);
+max14906_uart_remove:
 	no_os_uart_remove(uart_desc);
+
 #endif
 
 #if (BASIC_EXAMPLE + IIO_EXAMPLE == 0)

projects/max14906/src/platform/maxim/parameters.h
@@ -42,6 +42,7 @@
 #include "maxim_irq.h"
 #include "maxim_spi.h"
 #include "maxim_gpio.h"
+#include "maxim_gpio_irq.h"
 #include "maxim_uart.h"
 #include "maxim_uart_stdio.h"
 
@@ -80,6 +81,7 @@ extern struct max_spi_init_param max14906_spi_extra;
 #define GPIO_OPS	&max_gpio_ops
 #define GPIO_EXTRA	&max14906_gpio_extra_ip
 #define GPIO_IRQ_OPS	&max_gpio_irq_ops
+#define GPIO_IRQ_EXTRA	NULL
 extern struct max_gpio_init_param max14906_gpio_extra_ip;
 #if (TARGET_NUM == 32665)
 #define GPIO_FAULT_PORT_NUM	0

commit 69a44f3c6c536cfd0cad50cd5e4f25dafbe14656

projects: max14906 : Fix maxim interrupt handling for MAX14906 project
NVIC controller is not being properly initialized for the maxim
platform and callback function isn't called when it should.

Fix maxim/main.c to properly handle the NVIC controller for maxim
and also make changes in the basic example to suit both maxim and
STM platforms.

Fixes: e884e28 ("projects: max14906 : Change interrupt support for MAXIM platform")
Signed-off-by: Radu Sabau <radu.sabau@analog.com>

projects/max14906/src/examples/basic/basic_example.c
@@ -71,27 +71,6 @@ int basic_example_main()
 	enum max14906_climit climit = MAX14906_CL_300;
 	enum max14906_climit climit2 = MAX14906_CL_600;
 
-	/** MAX14906 Initialization */
-	ret = max14906_init(&max14906_desc, &max14906_ip);
-	if (ret)
-		goto exit;
-
-	/** Global Interrupt Controller */
-	struct no_os_irq_ctrl_desc *global_desc;
-	struct no_os_irq_init_param global_desc_param = {
-		.irq_ctrl_id = GPIO_IRQ_ID,
-		.platform_ops = GPIO_IRQ_OPS,
-		.extra = GPIO_IRQ_EXTRA
-	};
-
-	ret = no_os_irq_ctrl_init(&global_desc, &global_desc_param);
-	if (ret)
-		goto max14906_remove;
-
-	ret = no_os_irq_set_priority(global_desc, GPIO_IRQ_ID, 1);
-	if (ret)
-		goto remove_gpio_irq_ctrl;
-
 	struct no_os_callback_desc gpio_cb = {
 		.callback = gpio_callback_fn,
 		/** Parameter to be passed when the callback is called. */
@@ -101,17 +80,38 @@ int basic_example_main()
 		.handle = NULL
 	};
 
-	ret = no_os_irq_register_callback(global_desc, GPIO_IRQ_ID,
+	/** GPIO Pin Interrupt Controller */
+	struct no_os_irq_ctrl_desc *gpio_irq_desc;
+	struct no_os_irq_init_param gpio_irq_desc_param = {
+		.irq_ctrl_id = GPIO_IRQ_ID,
+		.platform_ops = GPIO_IRQ_OPS,
+		.extra = GPIO_IRQ_EXTRA
+	};
+
+	/** MAX14906 Initialization */
+	ret = max14906_init(&max14906_desc, &max14906_ip);
+	if (ret)
+		goto exit;
+
+	ret = no_os_irq_ctrl_init(&gpio_irq_desc, &gpio_irq_desc_param);
+	if (ret)
+		goto max14906_remove;
+
+	ret = no_os_irq_register_callback(gpio_irq_desc, GPIO_FAULT_PIN_NUM,
 					  &gpio_cb);
 	if (ret)
 		goto remove_gpio_irq_ctrl;
 
-	ret = no_os_irq_trigger_level_set(global_desc, GPIO_IRQ_ID,
+	ret = no_os_irq_trigger_level_set(gpio_irq_desc, GPIO_FAULT_PIN_NUM,
 					  NO_OS_IRQ_EDGE_FALLING);
 	if (ret)
 		goto gpio_irq_unregister_callback;
 
-	ret = no_os_irq_enable(global_desc, GPIO_IRQ_ID);
+	ret = no_os_irq_set_priority(gpio_irq_desc, GPIO_FAULT_PIN_NUM, 1);
+	if (ret)
+		goto gpio_irq_unregister_callback;
+
+	ret = no_os_irq_enable(gpio_irq_desc, GPIO_FAULT_PIN_NUM);
 	if (ret)
 		goto gpio_irq_unregister_callback;
 
@@ -166,9 +166,9 @@ int basic_example_main()
 	while (1);
 
 gpio_irq_unregister_callback:
-	no_os_irq_unregister_callback(global_desc, GPIO_FAULT_PIN_NUM, &gpio_cb);
+	no_os_irq_unregister_callback(gpio_irq_desc, GPIO_FAULT_PIN_NUM, &gpio_cb);
 remove_gpio_irq_ctrl:
-	no_os_irq_ctrl_remove(global_desc);
+	no_os_irq_ctrl_remove(gpio_irq_desc);
 max14906_remove:
 	max14906_remove(max14906_desc);
 exit:

projects/max14906/src/platform/maxim/main.c
@@ -58,22 +58,22 @@ int main()
 
 #ifdef BASIC_EXAMPLE
 	struct no_os_uart_desc *uart_desc;
-	struct no_os_irq_ctrl_desc *gpio_irq_desc;
-	struct no_os_irq_init_param gpio_irq_desc_param = {
-		.irq_ctrl_id = GPIO_IRQ_ID,
-		.platform_ops = GPIO_IRQ_OPS,
-		.extra = NULL
+
+	/* NVIC Interrupt Controller specific for Maxim platform. */
+	struct no_os_irq_ctrl_desc *nvic_desc;
+	struct no_os_irq_init_param nvic_desc_param = {
+		.platform_ops = &max_irq_ops,
 	};
 
 	ret = no_os_uart_init(&uart_desc, &max14906_uart_ip);
 	if (ret)
 		return ret;
 
-	ret = no_os_irq_ctrl_init(&gpio_irq_desc, &gpio_irq_desc_param);
+	ret = no_os_irq_ctrl_init(&nvic_desc, &nvic_desc_param);
 	if (ret)
 		goto max14906_uart_remove;
 
-	ret = no_os_irq_enable(gpio_irq_desc, GPIO0_IRQn);
+	ret = no_os_irq_enable(nvic_desc, NVIC_GPIO_IRQ);
 	if (ret)
 		goto max14906_irq_ctrl_remove;
 
@@ -82,7 +82,7 @@ int main()
 	ret = basic_example_main();
 
 max14906_irq_ctrl_remove:
-	no_os_irq_ctrl_remove(gpio_irq_desc);
+	no_os_irq_ctrl_remove(nvic_desc);
 max14906_uart_remove:
 	no_os_uart_remove(uart_desc);
 

projects/max14906/src/platform/maxim/parameters.h
@@ -82,15 +82,18 @@ extern struct max_spi_init_param max14906_spi_extra;
 #define GPIO_EXTRA	&max14906_gpio_extra_ip
 #define GPIO_IRQ_OPS	&max_gpio_irq_ops
 #define GPIO_IRQ_EXTRA	NULL
+
 extern struct max_gpio_init_param max14906_gpio_extra_ip;
 #if (TARGET_NUM == 32665)
 #define GPIO_FAULT_PORT_NUM	0
 #define GPIO_FAULT_PIN_NUM	5
 #define GPIO_IRQ_ID		0
+#define NVIC_GPIO_IRQ		GPIO0_IRQn
 #elif (TARGET_NUM == 32690)
 #define GPIO_FAULT_PORT_NUM	2
 #define GPIO_FAULT_PIN_NUM	21
 #define GPIO_IRQ_ID		2
+#define NVIC_GPIO_IRQ		GPIO2_IRQn
 #endif
 #endif
 