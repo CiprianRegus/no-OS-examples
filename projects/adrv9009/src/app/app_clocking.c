CiprianRegus/no-OS-examples

commit 43b68ec355841d91634bd8e6fc7d4074ad4016ca

Carrier hmc7044 clock config for dual adrv9009
Perform the carrier hmc7044 configuration to match the linux device tree
configuration.

Signed-off-by: Darius Berghe <darius.berghe@analog.com>

projects/adrv9009/src/app/app_clocking.c
@@ -81,6 +81,7 @@
 
 #if defined(ZU11EG)
 struct hmc7044_dev* clkchip_device;
+struct hmc7044_dev * car_clkchip_device;
 #else
 struct ad9528_dev* clkchip_device;
 #endif
@@ -191,6 +192,37 @@ adiHalErr_t clocking_init(uint32_t rx_div40_rate_hz,
 		.high_performance_mode_pll_vco_en = true,
 		.pulse_gen_mode = 0x0,
 		.channels = chan_spec
+	};
+
+	struct hmc7044_chan_spec car_chan_spec[2] = {
+		/* REFCLK_OUT2 */
+		{
+			.disable = 0, .num = 2, .divider = 24, .driver_mode = 1,
+		},
+		/* SYNC_OUT1 */
+		{
+			.disable = 0, .num = 5, .divider = 3840, .driver_mode = 3,
+			.start_up_mode_dynamic_enable = true,
+			.high_performance_mode_dis = true,
+			.driver_impedance = 3
+		},
+	};
+
+	struct hmc7044_init_param hmc7044_car_param = {
+		.spi_init = NULL,
+		.clkin_freq = {122880000, 122880000, 0, 19200000},
+		.vcxo_freq = 122880000,
+		.pll2_freq = 2949120000,
+		.pll1_loop_bw = 200,
+		.sysref_timer_div = 3840,
+		.in_buf_mode = {0x07, 0x07, 0x00, 0x11, 0x15},
+		.gpi_ctrl = {0x00, 0x00, 0x00, 0x11},
+		.gpo_ctrl = {0x1f, 0x2b, 0x00, 0x00},
+		.num_channels = 2,
+		.pll1_ref_prio_ctrl = 0xB1,
+		.sync_pin_mode = 0x1,
+		.pulse_gen_mode = 0x1,
+		.channels = car_chan_spec
 	};
 #else
 	struct ad9528_channel_spec ad9528_channels[14];
@@ -304,7 +336,8 @@ adiHalErr_t clocking_init(uint32_t rx_div40_rate_hz,
 #else
 		.type = SPI_PS,
 #endif
-		.device_id = 0
+		.device_id = 0,
+		.flags = SPI_CS_DECODE
 	};
 #if !defined(ZU11EG)
 	struct xil_gpio_init_param xil_gpio_param = {
@@ -342,6 +375,15 @@ adiHalErr_t clocking_init(uint32_t rx_div40_rate_hz,
 	};
 
 #if defined(ZU11EG)
+	// clock chip spi settings
+	struct spi_init_param car_clkchip_spi_init_param = {
+		.max_speed_hz = 10000000,
+		.mode = SPI_MODE_0,
+		.chip_select = CAR_CLK_CS,
+		.extra = &xil_spi_param
+	};
+	hmc7044_car_param.spi_init = &car_clkchip_spi_init_param;
+
 	hmc7044_param.spi_init = &clkchip_spi_init_param;
 #else
 	ad9528_param.spi_init = clkchip_spi_init_param;
@@ -362,6 +404,11 @@ adiHalErr_t clocking_init(uint32_t rx_div40_rate_hz,
 	* to the Talise device.
 	**/
 #if defined(ZU11EG)
+	status = hmc7044_init(&car_clkchip_device, &hmc7044_car_param);
+	if (status != SUCCESS) {
+		printf("hmc7044_init() error: %d\n", status);
+		goto error_1;
+	}
 	status = hmc7044_init(&clkchip_device, &hmc7044_param);
 	if (status != SUCCESS) {
 		printf("hmc7044_init() error: %d\n", status);

projects/adrv9009/src/devices/adi_hal/parameters.h
@@ -104,9 +104,10 @@
 #endif
 
 #if defined(ZU11EG) // ZU11EG
-#define CLK_CS				2
-#define ADRV_CS				0
-#define ADRV_B_CS			1
+#define ADRV_CS				0 // Talise A 
+#define ADRV_B_CS			1 // Talise B
+#define CLK_CS				2 // Clock chip placed on the som
+#define CAR_CLK_CS			3 // Clock chip placed on the carrier
 
 // Transceiver A
 #define TRX_A_RESETB_GPIO		130

commit 7b4d2edac3f6e4a40738617a542b00c11b9948cc

adrv9009: src: app: app_clocking.c Set clock source
Set correct clock sources in the ADRV9009-ZU11EG project.

Fixes: 43b68ec ("Carrier hmc7044 clock config for dual adrv9009")
Signed-off-by: George Mois <george.mois@analog.com>

projects/adrv9009/src/app/app_clocking.c
@@ -114,7 +114,7 @@ adiHalErr_t clocking_init(uint32_t rx_div40_rate_hz,
 #if defined(ZU11EG) || defined(FMCOMMS8_ZCU102)
 	struct hmc7044_init_param hmc7044_param = {
 		.spi_init = NULL,
-		.clkin_freq = {122880000, 122880000, 0, 0},
+		.clkin_freq = {30720000, 30720000, 0, 0},
 		.vcxo_freq = 122880000,
 		.pll2_freq = 2949120000,
 		.pll1_loop_bw = 200,
@@ -244,7 +244,7 @@ adiHalErr_t clocking_init(uint32_t rx_div40_rate_hz,
 
 	struct hmc7044_init_param hmc7044_car_param = {
 		.spi_init = NULL,
-		.clkin_freq = {122880000, 122880000, 0, 19200000},
+		.clkin_freq = {122880000, 30720000, 0, 38400000},
 		.vcxo_freq = 122880000,
 		.pll2_freq = 2949120000,
 		.pll1_loop_bw = 200,