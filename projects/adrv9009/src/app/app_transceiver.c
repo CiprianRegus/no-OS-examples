CiprianRegus/no-OS-examples

commit d57d0ae3f6ed3f001bc71866bf4a47042d04445e

projects:adrv9009:add support for adrv9008-1/2
Support the ADRV9008-1 and ADRV9008-2 evaluation boards.

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

projects/adrv9009/src/app/app_transceiver.c
@@ -132,11 +132,14 @@ adiHalErr_t fpga_xcvr_init(uint32_t rx_lane_rate_khz,
 #endif
 
 	/* Initialize ADXCR */
+#ifndef ADRV9008_2
 	status = adxcvr_init(&rx_adxcvr, &rx_adxcvr_init);
 	if (status != SUCCESS) {
 		printf("error: %s: adxcvr_init() failed\n", rx_adxcvr_init.name);
 		goto error_0;
 	}
+#endif
+#ifndef ADRV9008_1
 	status = adxcvr_init(&tx_adxcvr, &tx_adxcvr_init);
 	if (status != SUCCESS) {
 		printf("error: %s: adxcvr_init() failed\n", tx_adxcvr_init.name);
@@ -147,12 +150,16 @@ adiHalErr_t fpga_xcvr_init(uint32_t rx_lane_rate_khz,
 		printf("error: %s: adxcvr_init() failed\n", rx_os_adxcvr_init.name);
 		goto error_9;
 	}
+#endif
 #ifndef ALTERA_PLATFORM
+#ifndef ADRV9008_2
 	status = adxcvr_clk_enable(rx_adxcvr);
 	if (status != SUCCESS) {
 		printf("error: %s: adxcvr_clk_enable() failed\n", rx_adxcvr->name);
 		goto error_10;
 	}
+#endif
+#ifndef ADRV9008_1
 	status = adxcvr_clk_enable(tx_adxcvr);
 	if (status != SUCCESS) {
 		printf("error: %s: adxcvr_clk_enable() failed\n", tx_adxcvr->name);
@@ -164,24 +171,35 @@ adiHalErr_t fpga_xcvr_init(uint32_t rx_lane_rate_khz,
 		goto error_10;
 	}
 #endif
+#endif
 
 	return ADIHAL_OK;
 
 #ifndef ALTERA_PLATFORM
 error_10:
+#ifndef ADRV9008_1
 	adxcvr_remove(rx_os_adxcvr);
 #endif
+#endif
 error_9:
+#ifndef ADRV9008_1
 	adxcvr_remove(tx_adxcvr);
+#endif
 error_8:
+#ifndef ADRV9008_2
 	adxcvr_remove(rx_adxcvr);
+#endif
 error_0:
 	return ADIHAL_ERR;
 }
 
 void fpga_xcvr_deinit(void)
 {
+#ifndef ADRV9008_1
 	adxcvr_remove(rx_os_adxcvr);
 	adxcvr_remove(tx_adxcvr);
+#endif
+#ifndef ADRV9008_1
 	adxcvr_remove(rx_adxcvr);
+#endif
 }

projects/adrv9009/src/app/app_clocking.c
@@ -573,6 +573,7 @@ adiHalErr_t clocking_init(uint32_t rx_div40_rate_hz,
 	}
 
 #ifdef ALTERA_PLATFORM
+#ifndef ADRV9008_2
 	/* Initialize A10 FPLLs */
 	status = altera_a10_fpll_init(&rx_device_clk_pll,
 				      &rx_device_clk_pll_init);
@@ -581,6 +582,8 @@ adiHalErr_t clocking_init(uint32_t rx_div40_rate_hz,
 		       rx_device_clk_pll_init.name);
 		goto error_1;
 	}
+#endif
+#ifndef ADRV9008_1
 	status = altera_a10_fpll_init(&tx_device_clk_pll,
 				      &tx_device_clk_pll_init);
 	if (status != SUCCESS) {
@@ -596,6 +599,8 @@ adiHalErr_t clocking_init(uint32_t rx_div40_rate_hz,
 		goto error_3;
 	}
 
+#endif
+#ifndef ADRV9008_2
 	altera_a10_fpll_disable(rx_device_clk_pll);
 	status = altera_a10_fpll_set_rate(rx_device_clk_pll,
 					  rx_div40_rate_hz);
@@ -605,6 +610,8 @@ adiHalErr_t clocking_init(uint32_t rx_div40_rate_hz,
 		goto error_4;
 	}
 	altera_a10_fpll_enable(rx_device_clk_pll);
+#endif
+#ifndef ADRV9008_1
 	altera_a10_fpll_disable(tx_device_clk_pll);
 	status = altera_a10_fpll_set_rate(tx_device_clk_pll,
 					  tx_div40_rate_hz);
@@ -623,14 +630,18 @@ adiHalErr_t clocking_init(uint32_t rx_div40_rate_hz,
 		goto error_4;
 	}
 	altera_a10_fpll_enable(rx_os_device_clk_pll);
+#endif
 #else
 #if !defined(ZU11EG) && !defined(FMCOMMS8_ZCU102)
 	/* Initialize CLKGEN */
+#ifndef ADRV9008_2
 	status = axi_clkgen_init(&rx_clkgen, &rx_clkgen_init);
 	if (status != SUCCESS) {
 		printf("error: %s: axi_clkgen_init() failed\n", rx_clkgen_init.name);
 		goto error_1;
 	}
+#endif
+#ifndef ADRV9008_1
 	status = axi_clkgen_init(&tx_clkgen, &tx_clkgen_init);
 	if (status != SUCCESS) {
 		printf("error: %s: axi_clkgen_init() failed\n", tx_clkgen_init.name);
@@ -642,11 +653,15 @@ adiHalErr_t clocking_init(uint32_t rx_div40_rate_hz,
 		goto error_3;
 	}
 
+#endif
+#ifndef ADRV9008_2
 	status = axi_clkgen_set_rate(rx_clkgen, rx_div40_rate_hz);
 	if (status != SUCCESS) {
 		printf("error: %s: axi_clkgen_set_rate() failed\n", rx_clkgen->name);
 		goto error_4;
 	}
+#endif
+#ifndef ADRV9008_1
 	status = axi_clkgen_set_rate(tx_clkgen, tx_div40_rate_hz);
 	if (status != SUCCESS) {
 		printf("error: %s: axi_clkgen_set_rate() failed\n", tx_clkgen->name);
@@ -659,6 +674,7 @@ adiHalErr_t clocking_init(uint32_t rx_div40_rate_hz,
 	}
 #endif
 #endif
+#endif
 
 	return ADIHAL_OK;
 
@@ -666,27 +682,39 @@ adiHalErr_t clocking_init(uint32_t rx_div40_rate_hz,
 error_4:
 #endif
 #ifdef ALTERA_PLATFORM
+#ifndef ADRV9008_1
 	altera_a10_fpll_remove(rx_os_device_clk_pll);
+#endif
 #elif !defined(ZU11EG) && !defined(FMCOMMS8_ZCU102)
+#ifndef ADRV9008_1
 	axi_clkgen_remove(rx_os_clkgen);
+#endif
 #endif
 
 #if !defined(ZU11EG) && !defined(FMCOMMS8_ZCU102)
 error_3:
 #endif
 #ifdef ALTERA_PLATFORM
+#ifndef ADRV9008_1
 	altera_a10_fpll_remove(tx_device_clk_pll);
+#endif
 #elif !defined(ZU11EG) && !defined(FMCOMMS8_ZCU102)
+#ifndef ADRV9008_1
 	axi_clkgen_remove(tx_clkgen);
+#endif
 #endif
 
 #if !defined(ZU11EG) && !defined(FMCOMMS8_ZCU102)
 error_2:
 #endif
 #ifdef ALTERA_PLATFORM
+#ifndef ADRV9008_2
 	altera_a10_fpll_remove(rx_device_clk_pll);
+#endif
 #elif !defined(ZU11EG) && !defined(FMCOMMS8_ZCU102)
+#ifndef ADRV9008_2
 	axi_clkgen_remove(rx_clkgen);
+#endif
 #endif
 error_1:
 #if defined(ZU11EG) || defined(FMCOMMS8_ZCU102)
@@ -701,13 +729,21 @@ error_0:
 void clocking_deinit(void)
 {
 #ifdef ALTERA_PLATFORM
+#ifndef ADRV9008_2
 	altera_a10_fpll_remove(rx_device_clk_pll);
+#endif
+#ifndef ADRV9008_1
 	altera_a10_fpll_remove(tx_device_clk_pll);
 	altera_a10_fpll_remove(rx_os_device_clk_pll);
+#endif
 #elif !defined(ZU11EG) && !defined(FMCOMMS8_ZCU102)
+#ifndef ADRV9008_1
 	axi_clkgen_remove(rx_os_clkgen);
 	axi_clkgen_remove(tx_clkgen);
+#endif
+#ifndef ADRV9008_2
 	axi_clkgen_remove(rx_clkgen);
+#endif
 #endif
 
 #if defined(ZU11EG) || defined(FMCOMMS8_ZCU102)

projects/adrv9009/src/app/app_config.h
@@ -50,6 +50,10 @@
  * by default for zc706 + adrv9009-w/pcbz.
  */
 
+/* To build ADRV9008-1/ADRV9008-2, uncomment one (only one) of the lines below: */
+//#define ADRV9008_1
+//#define ADRV9008_2
+
 /* To build a specific example, uncomment one (only one) of the lines below: */
 // #define DAC_DMA_EXAMPLE
 // #define IIO_SUPPORT

projects/adrv9009/src/app/app_jesd.c
@@ -104,11 +104,14 @@ adiHalErr_t jesd_init(uint32_t rx_div40_rate_hz,
 	};
 
 	/* Initialize JESD */
+#ifndef ADRV9008_2
 	status = axi_jesd204_rx_init(&rx_jesd, &rx_jesd_init);
 	if (status != SUCCESS) {
 		printf("error: %s: axi_jesd204_rx_init() failed\n", rx_jesd_init.name);
 		goto error_5;
 	}
+#endif
+#ifndef ADRV9008_1
 	status = axi_jesd204_tx_init(&tx_jesd, &tx_jesd_init);
 	if (status != SUCCESS) {
 		printf("error: %s: axi_jesd204_tx_init() failed\n", tx_jesd_init.name);
@@ -120,35 +123,54 @@ adiHalErr_t jesd_init(uint32_t rx_div40_rate_hz,
 		goto error_7;
 	}
 
+#endif
 	return ADIHAL_OK;
 
 error_7:
+#ifndef ADRV9008_1
 	axi_jesd204_rx_remove(rx_os_jesd);
+#endif
 error_6:
+#ifndef ADRV9008_1
 	axi_jesd204_tx_remove(tx_jesd);
+#endif
 error_5:
+#ifndef ADRV9008_2
 	axi_jesd204_rx_remove(rx_jesd);
+#endif
 
 	return ADIHAL_ERR;
 }
 
 void jesd_deinit(void)
 {
+#ifndef	ADRV9008_1
 	axi_jesd204_rx_remove(rx_os_jesd);
 	axi_jesd204_tx_remove(tx_jesd);
+#endif
+#ifndef ADRV9008_2
 	axi_jesd204_rx_remove(rx_jesd);
+#endif
 }
 
 void jesd_status(void)
 {
+#ifndef ADRV9008_2
 	axi_jesd204_rx_status_read(rx_jesd);
+#endif
+#ifndef ADRV9008_1
 	axi_jesd204_tx_status_read(tx_jesd);
 	axi_jesd204_rx_status_read(rx_os_jesd);
+#endif
 }
 
 void jesd_rx_watchdog(void)
 {
+#ifndef ADRV9008_2
 	axi_jesd204_rx_watchdog(rx_jesd);
+#endif
+#ifndef ADRV9008_1
 	axi_jesd204_rx_watchdog(rx_os_jesd);
+#endif
 }
 

projects/adrv9009/src/app/app_talise.c
@@ -64,6 +64,7 @@
 #include "axi_jesd204_tx.h"
 
 // hal
+#include "app_config.h"
 #include "parameters.h"
 #include "adi_hal.h"
 
@@ -93,12 +94,23 @@ adiHalErr_t talise_setup(taliseDevice_t * const pd, taliseInit_t * const pi)
 	uint8_t framerStatus = 0;
 	uint32_t count = sizeof(armBinary);
 	taliseArmVersionInfo_t talArmVersionInfo;
+#if defined(ADRV9008_1)
+	uint32_t initCalMask = TAL_ADC_TUNER | TAL_TIA_3DB_CORNER | TAL_DC_OFFSET |
+			       TAL_RX_GAIN_DELAY | TAL_FLASH_CAL | TAL_RX_QEC_INIT;
+#elif defined(ADRV9008_2)
+	uint32_t initCalMask = TAL_TX_BB_FILTER | TAL_ADC_TUNER | TAL_TIA_3DB_CORNER |
+			       TAL_DC_OFFSET | TAL_FLASH_CAL | TAL_PATH_DELAY |
+			       TAL_TX_LO_LEAKAGE_INTERNAL | TAL_TX_QEC_INIT |
+			       TAL_LOOPBACK_RX_LO_DELAY | TAL_LOOPBACK_RX_RX_QEC_INIT |
+			       TAL_ORX_QEC_INIT | TAL_TX_DAC  | TAL_ADC_STITCHING;
+#else
 	uint32_t initCalMask =  TAL_TX_BB_FILTER | TAL_ADC_TUNER |  TAL_TIA_3DB_CORNER |
 				TAL_DC_OFFSET | TAL_RX_GAIN_DELAY | TAL_FLASH_CAL |
 				TAL_PATH_DELAY | TAL_TX_LO_LEAKAGE_INTERNAL |
 				TAL_TX_QEC_INIT | TAL_LOOPBACK_RX_LO_DELAY |
 				TAL_LOOPBACK_RX_RX_QEC_INIT | TAL_RX_QEC_INIT |
 				TAL_ORX_QEC_INIT | TAL_TX_DAC  | TAL_ADC_STITCHING;
+#endif
 	uint32_t trackingCalMask =  TAL_TRACK_RX1_QEC |
 				    TAL_TRACK_RX2_QEC |
 				    TAL_TRACK_TX1_QEC |

projects/adrv9009/src/app/headless.c
@@ -52,6 +52,7 @@ int32_t start_iiod(struct axi_dmac *rx_dmac, struct axi_dmac *tx_dmac,
 	struct iio_device		*dac_dev_desc;
 	int32_t				status;
 
+#ifndef ADRV9008_2
 	iio_axi_adc_init_par = (struct iio_axi_adc_init_param) {
 		.rx_adc = rx_adc,
 		.rx_dmac = rx_dmac,
@@ -60,6 +61,8 @@ int32_t start_iiod(struct axi_dmac *rx_dmac, struct axi_dmac *tx_dmac,
 						     uint32_t))Xil_DCacheInvalidateRange
 #endif
 	};
+#endif
+#ifndef ADRV9008_1
 	iio_axi_dac_init_par = (struct iio_axi_dac_init_param) {
 		.tx_dac = tx_dac,
 		.tx_dmac = tx_dmac,
@@ -67,32 +70,49 @@ int32_t start_iiod(struct axi_dmac *rx_dmac, struct axi_dmac *tx_dmac,
 		.dcache_flush_range = (void (*)(uint32_t, uint32_t))Xil_DCacheFlushRange,
 #endif
 	};
+#endif
 
+#ifndef ADRV9008_2
 	status = iio_axi_adc_init(&iio_axi_adc_desc, &iio_axi_adc_init_par);
 	if (status < 0)
 		return status;
+#endif
 
+#ifndef ADRV9008_1
 	status = iio_axi_dac_init(&iio_axi_dac_desc, &iio_axi_dac_init_par);
 	if(status < 0)
 		return status;
+#endif
 
+#ifndef ADRV9008_2
 	iio_axi_adc_get_dev_descriptor(iio_axi_adc_desc, &adc_dev_desc);
+#endif
+#ifndef ADRV9008_1
 	iio_axi_dac_get_dev_descriptor(iio_axi_dac_desc, &dac_dev_desc);
+#endif
 
+#ifndef ADRV9008_2
 	struct iio_data_buffer read_buff = {
 		.buff = (void *)ADC_DDR_BASEADDR,
 		.size = 0xFFFFFFFF,
 	};
+#endif
+#ifndef ADRV9008_1
 	struct iio_data_buffer write_buff = {
 		.buff = (void *)DAC_DDR_BASEADDR,
 		.size = 0xFFFFFFFF,
 	};
+#endif
 
 	struct iio_app_device devices[] = {
+#ifndef ADRV9008_2
 		IIO_APP_DEVICE("axi_adc", iio_axi_adc_desc, adc_dev_desc,
 			       &read_buff, NULL),
+#endif
+#ifndef ADRV9008_1
 		IIO_APP_DEVICE("axi_dac", iio_axi_dac_desc, dac_dev_desc,
 			       NULL, &write_buff)
+#endif
 	};
 
 	return iio_app_run(devices, ARRAY_SIZE(devices));
@@ -279,30 +299,38 @@ int main(void)
 	jesd_status();
 
 	/* Initialize the DAC core */
+#ifndef ADRV9008_1
 	status = axi_dac_init(&tx_dac, &tx_dac_init);
 	if (status) {
 		printf("axi_dac_init() failed with status %d\n", status);
 		goto error_3;
 	}
+#endif
 
 	/* Initialize the ADC core */
+#ifndef ADRV9008_2
 	status = axi_adc_init(&rx_adc, &rx_adc_init);
 	if (status) {
 		printf("axi_adc_init() failed with status %d\n", status);
 		goto error_3;
 	}
 
+#endif
+#ifndef ADRV9008_1
 	status = axi_dmac_init(&tx_dmac, &tx_dmac_init);
 	if (status) {
 		printf("axi_dmac_init() tx init error: %d\n", status);
 		goto error_3;
 	}
 
+#endif
+#ifndef ADRV9008_2
 	status = axi_dmac_init(&rx_dmac, &rx_dmac_init);
 	if (status) {
 		printf("axi_dmac_init() rx init error: %d\n", status);
 		goto error_3;
 	}
+#endif
 
 #ifdef DAC_DMA_EXAMPLE
 	gpio_init_plddrbypass.extra = &hal_gpio_param;
@@ -319,6 +347,7 @@ int main(void)
 	}
 	gpio_direction_output(gpio_plddrbypass, 0);
 
+#ifndef ADRV9008_1
 	axi_dac_load_custom_data(tx_dac, sine_lut_iq,
 				 ARRAY_SIZE(sine_lut_iq),
 				 DAC_DDR_BASEADDR);
@@ -329,8 +358,10 @@ int main(void)
 	axi_dmac_transfer(tx_dmac, DAC_DDR_BASEADDR, sizeof(sine_lut_iq));
 
 	mdelay(1000);
+#endif
 
 	/* Transfer 16384 samples from ADC to MEM */
+#ifndef ADRV9008_2
 	axi_dmac_transfer(rx_dmac,
 			  DDR_MEM_BASEADDR + 0x800000,
 			  16384 * TALISE_NUM_CHANNELS *
@@ -339,6 +370,7 @@ int main(void)
 	Xil_DCacheInvalidateRange(DDR_MEM_BASEADDR + 0x800000,
 				  16384 * TALISE_NUM_CHANNELS *
 				  DIV_ROUND_UP(talInit.jesd204Settings.framerA.Np, 8));
+#endif
 #endif
 #endif
 

commit bfd412c414806f001765d121ef6d5eea26829890

adrv9009: src: app: app_transceiver.c: Fix deinit
Set the deinit function to remove the rx_adxcvr component for
ADRV9009 and ADRV9008-1 devices (ADRV9008-2 does not have Rx).

Fixes: d57d0ae ("projects:adrv9009:add support for adrv9008-1/2")

Signed-off-by: George Mois <george.mois@analog.com>

projects/adrv9009/src/app/app_transceiver.c
@@ -196,7 +196,7 @@ void fpga_xcvr_deinit(void)
 	adxcvr_remove(rx_os_adxcvr);
 	adxcvr_remove(tx_adxcvr);
 #endif
-#ifndef ADRV9008_1
+#ifndef ADRV9008_2
 	adxcvr_remove(rx_adxcvr);
 #endif
 }