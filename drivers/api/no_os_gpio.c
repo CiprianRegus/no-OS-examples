d3a12056e72de32cdca65b654f79bcc36a2b2256
Ciprian Regus <ciprian.regus@analog.com>
2025-02-21 11:48:57 +0200

    change in both the repo and subtree

--- drivers/api/no_os_gpio.c
+++ drivers/api/no_os_gpio.c
@@ -30,7 +30,6 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *******************************************************************************/
-
 #include <inttypes.h>
 #include "no_os_gpio.h"
 #include <stdlib.h>
